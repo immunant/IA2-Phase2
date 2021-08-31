9dd35ab014219572ed1db157fa4818e213a7058a
Andrei Homescu <ah@immunant.com>
2021-08-30 19:54:16 -0700

    Merge IA2_ICALL_WRAPPER and IA2_ICALL_WRAPPED into a single macro

--- include/ia2.h
+++ include/ia2.h
@@ -9,13 +9,14 @@
 
 #define IA2_USED __attribute__((used))
 
-#define IA2_ICALL_WRAPPER(target, ty)  \
-  asm (                                \
-    "IA2_icall_wrapper_" #target ":\n" \
-    "call " #target "\n"               \
-    "ret\n"                            \
-  );                                   \
-  extern char IA2_icall_wrapper_##target[]
-
-#define IA2_ICALL_WRAPPED(target, ty) \
-  ((struct IA2_fnptr_##ty){ IA2_icall_wrapper_##target })
+#define IA2_ICALL_WRAPPER(target, ty) ({    \
+  asm (                                     \
+    ".pushsection .ia2.wrappers, \"ax\"\n"  \
+    "IA2_icall_wrapper_" #target ":\n"      \
+    "call " #target "\n"                    \
+    "ret\n"                                 \
+    ".popsection\n"                         \
+  );                                        \
+  extern char IA2_icall_wrapper_##target[]; \
+  (struct IA2_fnptr_##ty){ IA2_icall_wrapper_##target }; \
+})

697de0a2aab2cc4bba2f04c087403a42bfcd8893
Andrei Homescu <ah@immunant.com>
2021-08-30 19:02:42 -0700

    Add ia2.h header with macros for indirect call wrapping

--- include/ia2.h
+++ include/ia2.h
@@ -6,3 +6,16 @@
 #define IA2_WRAP_FUNCTION(name)                 \
     __asm__(".symver " #name ",__ia2_" #name "@IA2")
 #endif
+
+#define IA2_USED __attribute__((used))
+
+#define IA2_ICALL_WRAPPER(target, ty)  \
+  asm (                                \
+    "IA2_icall_wrapper_" #target ":\n" \
+    "call " #target "\n"               \
+    "ret\n"                            \
+  );                                   \
+  extern char IA2_icall_wrapper_##target[]
+
+#define IA2_ICALL_WRAPPED(target, ty) \
+  ((struct IA2_fnptr_##ty){ IA2_icall_wrapper_##target })

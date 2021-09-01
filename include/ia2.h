3ec45405e413ababf763714ec9fd2892a74f471f
Andrei Homescu <ah@immunant.com>
2021-08-31 20:09:44 -0700

    Redesign the indirect call wrapper macros to correctly write the return and argument types using Type::print which handles all types correctly, e.g., function pointer types

--- include/ia2.h
+++ include/ia2.h
@@ -9,24 +9,22 @@
 
 #define IA2_USED __attribute__((used))
 
-#define IA2_ICALL_WRAPPER(target, ty) ({            \
-  IA2_FNPTR_RETURN_##ty                             \
-  IA2_icall_wrapper_##target(IA2_FNPTR_ARGS_##ty) { \
-    IA2_FNPTR_RETURN_##ty __res =                   \
-      target(IA2_FNPTR_ARG_NAMES_##ty);             \
-    return __res;                                   \
-  }                                                 \
-  (struct IA2_fnptr_##ty){                          \
-    (char*)IA2_icall_wrapper_##target               \
-  };                                                \
+#define IA2_ICALL_WRAPPER(target, ty) ({               \
+  IA2_FNPTR_WRAPPER_##ty(IA2_icall_wrapper_##target) { \
+    IA2_FNPTR_RETURN_##ty(__res) =                     \
+      target(IA2_FNPTR_ARG_NAMES_##ty);                \
+    return __res;                                      \
+  }                                                    \
+  (struct IA2_fnptr_##ty){                             \
+    (char*)IA2_icall_wrapper_##target                  \
+  };                                                   \
 })
 
-#define IA2_ICALL_WRAPPER_VOID(target, ty) ({       \
-  void                                              \
-  IA2_icall_wrapper_##target(IA2_FNPTR_ARGS_##ty) { \
-    target(IA2_FNPTR_ARG_NAMES_##ty);             \
-  }                                                 \
-  (struct IA2_fnptr_##ty){                          \
-    (char*)IA2_icall_wrapper_##target               \
-  };                                                \
+#define IA2_ICALL_WRAPPER_VOID(target, ty) ({          \
+  IA2_FNPTR_WRAPPER_##ty(IA2_icall_wrapper_##target) { \
+    target(IA2_FNPTR_ARG_NAMES_##ty);                  \
+  }                                                    \
+  (struct IA2_fnptr_##ty){                             \
+    (char*)IA2_icall_wrapper_##target                  \
+  };                                                   \
 })

06127a3a83d472fdfd4e8bbae6343b12306156f8
Andrei Homescu <ah@immunant.com>
2021-08-31 19:10:29 -0700

    Emit the indirect call wrappers using nested C functions just like the direct call wrappers, instead of inline assembly code

--- include/ia2.h
+++ include/ia2.h
@@ -9,14 +9,24 @@
 
 #define IA2_USED __attribute__((used))
 
-#define IA2_ICALL_WRAPPER(target, ty) ({    \
-  asm (                                     \
-    ".pushsection .ia2.wrappers, \"ax\"\n"  \
-    "IA2_icall_wrapper_" #target ":\n"      \
-    "call " #target "\n"                    \
-    "ret\n"                                 \
-    ".popsection\n"                         \
-  );                                        \
-  extern char IA2_icall_wrapper_##target[]; \
-  (struct IA2_fnptr_##ty){ IA2_icall_wrapper_##target }; \
+#define IA2_ICALL_WRAPPER(target, ty) ({            \
+  IA2_FNPTR_RETURN_##ty                             \
+  IA2_icall_wrapper_##target(IA2_FNPTR_ARGS_##ty) { \
+    IA2_FNPTR_RETURN_##ty __res =                   \
+      target(IA2_FNPTR_ARG_NAMES_##ty);             \
+    return __res;                                   \
+  }                                                 \
+  (struct IA2_fnptr_##ty){                          \
+    (char*)IA2_icall_wrapper_##target               \
+  };                                                \
+})
+
+#define IA2_ICALL_WRAPPER_VOID(target, ty) ({       \
+  void                                              \
+  IA2_icall_wrapper_##target(IA2_FNPTR_ARGS_##ty) { \
+    target(IA2_FNPTR_ARG_NAMES_##ty);             \
+  }                                                 \
+  (struct IA2_fnptr_##ty){                          \
+    (char*)IA2_icall_wrapper_##target               \
+  };                                                \
 })

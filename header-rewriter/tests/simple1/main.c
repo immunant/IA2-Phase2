c56cd107377d9a875e467d09d0ddc8cf2d80d0e2
Andrei Homescu <ah@immunant.com>
2021-09-23 20:18:29 -0700

    Rename IA2_ICALL_WRAPPER to IA2_FNPTR_WRAPPER

--- header-rewriter/tests/simple1/main.c
+++ header-rewriter/tests/simple1/main.c
@@ -48,8 +48,8 @@ static int main_map(int x) {
 int main() {
   struct SimpleCallbacks scb = {
 #if MAIN_USE_IA2
-    .read_cb = IA2_ICALL_WRAPPER(main_read, _ZTSPFiiE),
-    .write_cb = IA2_ICALL_WRAPPER_VOID(main_write, _ZTSPFviE),
+    .read_cb = IA2_FNPTR_WRAPPER(main_read, _ZTSPFiiE),
+    .write_cb = IA2_FNPTR_WRAPPER_VOID(main_write, _ZTSPFviE),
 #else
     .read_cb = main_read,
     .write_cb = main_write,
@@ -64,8 +64,8 @@ int main() {
 
   srand(time(NULL));
 #if MAIN_USE_IA2
-  simple_foreach_v1(s, IA2_ICALL_WRAPPER(main_map, _ZTSPFiiE));
-  simple_foreach_v2(s, IA2_ICALL_WRAPPER(main_map, _ZTSPFiiE));
+  simple_foreach_v1(s, IA2_FNPTR_WRAPPER(main_map, _ZTSPFiiE));
+  simple_foreach_v2(s, IA2_FNPTR_WRAPPER(main_map, _ZTSPFiiE));
 #else
   simple_foreach_v1(s, main_map);
   simple_foreach_v2(s, main_map);

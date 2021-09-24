af58e361a5e9cc7fd1d76a2a4fad897fe8bd4b67
Andrei Homescu <ah@immunant.com>
2021-09-23 20:18:29 -0700

    Rename IA2_ICALL_WRAPPER to IA2_FNPTR_WRAPPER

--- include/ia2.h
+++ include/ia2.h
@@ -7,24 +7,22 @@
     __asm__(".symver " #name ",__ia2_" #name "@IA2")
 #endif
 
-#define IA2_USED __attribute__((used))
-
-#define IA2_ICALL_WRAPPER(target, ty) ({               \
-  IA2_FNPTR_WRAPPER_##ty(IA2_icall_wrapper_##target) { \
+#define IA2_FNPTR_WRAPPER(target, ty) ({               \
+  IA2_FNPTR_WRAPPER_##ty(IA2_fnptr_wrapper_##target) { \
     IA2_FNPTR_RETURN_##ty(__res) =                     \
       target(IA2_FNPTR_ARG_NAMES_##ty);                \
     return __res;                                      \
   }                                                    \
   (struct IA2_fnptr_##ty){                             \
-    (char*)IA2_icall_wrapper_##target                  \
+    (char*)IA2_fnptr_wrapper_##target                  \
   };                                                   \
 })
 
-#define IA2_ICALL_WRAPPER_VOID(target, ty) ({          \
-  IA2_FNPTR_WRAPPER_##ty(IA2_icall_wrapper_##target) { \
+#define IA2_FNPTR_WRAPPER_VOID(target, ty) ({          \
+  IA2_FNPTR_WRAPPER_##ty(IA2_fnptr_wrapper_##target) { \
     target(IA2_FNPTR_ARG_NAMES_##ty);                  \
   }                                                    \
   (struct IA2_fnptr_##ty){                             \
-    (char*)IA2_icall_wrapper_##target                  \
+    (char*)IA2_fnptr_wrapper_##target                  \
   };                                                   \
 })

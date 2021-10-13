eb48afe9ab31a3853b745a1ebea627d2d7dd585b
Ayrton Munoz <adm@immunant.com>
2021-10-13 11:17:01 -0400

    Fix macro_attr test

--- header-rewriter/tests/macro_attr/functions.h
+++ header-rewriter/tests/macro_attr/functions.h
@@ -1,17 +1,17 @@
 /*
 RUN: cp %s %t.h
 RUN: ia2-header-rewriter %t.c %t.h --
-RUN: cat %t.h | FileCheck %s
+RUN: cat %t.h | sed 's/^.*CHECK.*$//' | FileCheck %s
 */
 #define ATTR __attribute__((hot))
 #define UNUSED __attribute__((unused))
 
-/* CHECK: IA2_WRAP_FUNCTION(f); */
+// CHECK: IA2_WRAP_FUNCTION(f);
 void f();
-/* CHECK: IA2_WRAP_FUNCTION(g); */
+// CHECK: IA2_WRAP_FUNCTION(g);
 ATTR void g();
 
-/* CHECK: typedef struct IA2_fnptr__ZTSPFiiE CB; */
+// CHECK: typedef struct IA2_fnptr__ZTSPFiiE CB;
 UNUSED typedef int (*CB)(int);
 
 void h(CB cb);

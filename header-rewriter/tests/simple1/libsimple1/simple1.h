b6785169d9269fdd66ec5424ec63249050cbedbf
Ayrton Munoz <adm@immunant.com>
2021-10-13 13:31:52 -0400

    Add FileCheck annotations to simple1 test
    Skipped hooks.h because it's currently not being copied to the cmake output
    directory. Adding this should be straightforward but I'd rather get CI working
    before mucking around with cmake again.

--- header-rewriter/tests/simple1/libsimple1/simple1.h
+++ header-rewriter/tests/simple1/libsimple1/simple1.h
@@ -1,17 +1,30 @@
+/*
+RUN: cp %s %t.h
+RUN: ia2-header-rewriter --output-header %T/simple1_ia2.h wrapper.c %t.h -- -I%resource_dir
+RUN: cat %t.h | sed 's/^.*CHECK.*$//' | FileCheck %s
+*/
 #pragma once
 
 struct Simple;
 
 struct SimpleCallbacks {
+  // CHECK: IA2_fnptr__ZTSPFiiE read_cb;
   int (*read_cb)(int);
+  // CHECK: IA2_fnptr__ZTSPFviE write_cb;
   void (*write_cb)(int);
 };
 
+// CHECK: typedef struct IA2_fnptr__ZTSPFiiE SimpleMapFn;
 typedef int (*SimpleMapFn)(int);
 
+// CHECK: IA2_WRAP_FUNCTION(simple_new);
 struct Simple *simple_new(struct SimpleCallbacks);
+// CHECK: IA2_WRAP_FUNCTION(simple_reset);
 void simple_reset(struct Simple*);
+// CHECK: IA2_WRAP_FUNCTION(simple_destroy);
 void simple_destroy(struct Simple*);
+// CHECK: IA2_WRAP_FUNCTION(simple_foreach_v1);
 void simple_foreach_v1(struct Simple*, int (*)(int));
+// CHECK: IA2_WRAP_FUNCTION(simple_foreach_v2);
 void simple_foreach_v2(struct Simple*, SimpleMapFn);
 

fa382d74072a7098015fe9429ed974871f3c3d5d
Ayrton Munoz <adm@immunant.com>
2021-10-13 11:21:25 -0400

    Fix header_includes test
    Since this test uses one header-rewriter invocation to rewrite multiple headers
    (impl.h and liboption.h) this fix required using a local LIT config file to
    specify the file with the LIT commands (main.c). This commit also uses sed to
    remove the FileCheck annotations from the rewritten header and fixed the type of
    comment used.

--- header-rewriter/tests/header_includes/impl.h
+++ header-rewriter/tests/header_includes/impl.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <types.h>
 
-/* CHECK: IA2_WRAP_FUNCTION(unwrap_or) */
+// CHECK: IA2_WRAP_FUNCTION(unwrap_or);
 int unwrap_or(Option opt, int default_value);
--- header-rewriter/tests/header_includes/liboption.h
+++ header-rewriter/tests/header_includes/liboption.h
@@ -2,8 +2,8 @@
 #include <types.h>
 #include <impl.h>
 
-/* CHECK: IA2_WRAP_FUNCTION(Some) */
+// CHECK: IA2_WRAP_FUNCTION(Some);
 Option Some(int x);
 
-/* CHECK: IA2_WRAP_FUNCTION(None) */
+// CHECK: IA2_WRAP_FUNCTION(None);
 Option None();

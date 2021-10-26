3742e6ddca30f6101d6310967e012e6c3bfd86a7
Frances Wingerter <fw@immunant.com>
2021-10-26 12:15:21 -0400

    do not overwrite test case wrapper files when running lit
    doing so made `ninja check-ia2` fail the second time when run twice

--- header-rewriter/tests/header_includes/main.c
+++ header-rewriter/tests/header_includes/main.c
@@ -1,6 +1,6 @@
 /*
 RUN: cp %S/include/liboption.h %S/include/types.h %S/include/impl.h .
-RUN: ia2-header-rewriter wrapper.c liboption.h types.h -- -I. -I%resource_dir
+RUN: ia2-header-rewriter %T/wrapper.c liboption.h types.h -- -I. -I%resource_dir
 RUN: cat liboption.h | sed 's/^.*CHECK.*$//' | FileCheck %S/include/liboption.h
 RUN: cat impl.h | sed 's/^.*CHECK.*$//' | FileCheck %S/include/impl.h
 */

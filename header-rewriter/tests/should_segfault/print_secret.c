fe3d35b4ad657f94947afee1bc911de10e1698d5
Ayrton Munoz <adm@immunant.com>
2021-11-01 17:00:04 -0400

    Use async-signal-safe `_exit` instead of `exit` in sighandler

--- header-rewriter/tests/should_segfault/print_secret.c
+++ header-rewriter/tests/should_segfault/print_secret.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 #include "print_secret.h"
 
 void handle_segfault(int sig) {
     printf("seg faulted as expected\n");
-    exit(0);
+    _exit(0);
 }
 
 void print_secret() {

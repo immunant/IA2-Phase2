e8663cdfbfbb323776093c7d2adde04cd8574b74
Ayrton Munoz <adm@immunant.com>
2021-11-02 12:57:57 -0400

    Add LIT annotations and async-signal-safe functions to should_segfault

--- header-rewriter/tests/should_segfault/include/print_secret.h
+++ header-rewriter/tests/should_segfault/include/print_secret.h
@@ -1,6 +1,13 @@
+/*
+RUN: cp %s %t.h
+RUN: ia2-header-rewriter %T/wrapper.c %t.h -- -I%resource_dir
+RUN: cat %t.h | sed 's/^.*CHECK.*$//' | FileCheck %s
+RUN: %binary_dir/tests/should_segfault/should_segfault-main | diff %S/../Output/should_segfault.out -
+*/
 #pragma once
 #include <stdint.h>
 
 extern uint32_t secret;
 
+// CHECK: IA2_WRAP_FUNCTION(print_secret);
 void print_secret();
--- header-rewriter/tests/should_segfault/print_secret.c
+++ header-rewriter/tests/should_segfault/print_secret.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 #include "print_secret.h"
 
 void handle_segfault(int sig) {
-    printf("seg faulted as expected\n");
-    _exit(0);
+    if (sig == SIGSEGV) {
+        // Write directly to stdout since printf is not async-signal-safe
+        const char *msg = "UNTRUSTED: seg faulted as expected\n";
+        write(1, msg, strlen(msg));
+        _exit(0);
+    }
 }
 
 void print_secret() {
     signal(SIGSEGV, handle_segfault);
-    printf("UNTRUSTED: the secret is %d\n", secret);
+    printf("UNTRUSTED: the secret is %x\n", secret);
 }

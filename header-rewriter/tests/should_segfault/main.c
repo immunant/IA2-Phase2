fcbe33b7f2e584a60e9d9f59553495c9dce74198
Ayrton Munoz <adm@immunant.com>
2021-11-02 12:57:57 -0400

    Add LIT annotations and async-signal-safe functions to should_segfault

--- header-rewriter/tests/should_segfault/main.c
+++ header-rewriter/tests/should_segfault/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdint.h>
 #include <print_secret.h>
 #include <ia2.h>
@@ -7,6 +9,10 @@ uint32_t secret = 0xdeadbeef;
 
 int main() {
     initialize_heap_pkey(NULL, 0);
-    printf("TRUSTED: the secret is %d\n", secret);
+    // This program should terminate in the sighandler so we should avoid
+    // using printf since flushing stdout is not async-signal-safe
+    char buf[40];
+    sprintf(buf, "TRUSTED: the secret is %x\n", secret);
+    write(1, buf, strlen(buf));
     print_secret();
 }

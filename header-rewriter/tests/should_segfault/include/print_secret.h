f7feb36d268fc57f1d4c1ddf8bf170bc143b32cb
Ayrton Munoz <adm@immunant.com>
2021-11-04 21:39:08 -0400

    Add LIBIA2_INSECURE cmake option for CI

--- header-rewriter/tests/should_segfault/include/print_secret.h
+++ header-rewriter/tests/should_segfault/include/print_secret.h
@@ -2,7 +2,7 @@
 RUN: cp %s %t.h
 RUN: ia2-header-rewriter %T/wrapper.c %t.h -- -I%resource_dir
 RUN: cat %t.h | sed 's/^.*CHECK.*$//' | FileCheck %s
-RUN: %binary_dir/tests/should_segfault/should_segfault-main | diff %S/../Output/should_segfault.insecure.out -
+RUN: %binary_dir/tests/should_segfault/should_segfault-main | diff %binary_dir/tests/should_segfault/should_segfault.out -
 */
 #pragma once
 #include <stdint.h>

f765f1c0f08f561f3cbac1b461e0784cdb3d0f96
Ayrton Munoz <adm@immunant.com>
2021-11-19 13:29:43 -0500

    Add linker script stress tests to untrusted_indirect/main.c

--- header-rewriter/tests/untrusted_indirect/main.c
+++ header-rewriter/tests/untrusted_indirect/main.c
@@ -5,7 +5,26 @@
 
 static uint64_t secret = 0xcafed00d;
 
-uint64_t pick_rhs(uint64_t x, uint64_t y) {
+// This test defines some variables, constants and functions in unique
+// sections as a way to stress test the linker script `padding.ld` which was
+// required to fix this test's behavior.
+uint32_t initialized_var __attribute__((section("my_var_section"))) = 0x11223344;
+const uint32_t immutable_var __attribute__((section("my_const_var_section"))) = 0x55667788;
+uint32_t uninit_var __attribute__((section("my_uninit_var_section")));
+
+__asm__(".section my_alloc_section, \"a\"\n\
+    .byte 0\n\
+    .previous");
+
+__asm__(".section my_write_section, \"w\"\n\
+    .byte 0\n\
+    .previous");
+
+__asm__(".section my_executable_section, \"x\"\n\
+    .byte 0\n\
+    .previous");
+
+__attribute__((section("my_fn_section"))) uint64_t pick_rhs(uint64_t x, uint64_t y) {
     return y;
 }
 

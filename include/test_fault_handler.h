1efa84f5a1aca3a37b3b9ccc693ea4b494a7a871
Ayrton Munoz <adm@immunant.com>
2022-01-20 14:54:44 -0500

    Replace enum fault_cause with bool in test_fault_handler.h

--- include/test_fault_handler.h
+++ include/test_fault_handler.h
@@ -6,40 +6,28 @@
 // Configure the signal handler to expect an mpk violation when `expr` is
 // evaluated. If `expr` doesn't trigger a fault, this macro manually raises a
 // fault with a different message.
-#define CHECK_VIOLATION(expr) ({       \
-    fault = EXPECTED_FAULT;            \
-    typeof(expr) _tmp = expr;          \
-    fault = NO_FAULT;                  \
-    raise(SIGSEGV);                    \
-    _tmp;                              \
-})                                     \
+#define CHECK_VIOLATION(expr) ({                                \
+    expect_fault = true;                                        \
+    typeof(expr) _tmp = expr;                                   \
+    printf("CHECK_VIOLATION: did not seg fault as expected\n"); \
+    exit(0);                                                    \
+    _tmp;                                                       \
+})                                                              \
 
-enum fault_cause {
-    EARLY_FAULT,
-    EXPECTED_FAULT,
-    NO_FAULT,
-};
-
-static enum fault_cause fault = EARLY_FAULT;
+static bool expect_fault = false;
 
 // The test output should be checked to see that the segfault occurred at the
 // expected place.
 void handle_segfault(int sig) {
     if (sig == SIGSEGV) {
         // Write directly to stdout since printf is not async-signal-safe
-        const char *ok_msg = "SIGNAL HANDLER: seg faulted as expected\n";
-        const char *early_fault_msg = "SIGNAL HANDLER: unexpected seg fault\n";
-        const char *no_fault_msg = "SIGNAL HANDLER: did not seg fault as expected\n";
-        const char *unknown_fault_msg = "SIGNAL HANDLER: hit unreachable case when checking the fault cause\n";
+        const char *ok_msg = "CHECK_VIOLATION: seg faulted as expected\n";
+        const char *early_fault_msg = "CHECK_VIOLATION: unexpected seg fault\n";
         const char *msg;
-        if (fault == EARLY_FAULT) {
-            msg = early_fault_msg;
-        } else if (fault == EXPECTED_FAULT) {
+        if (expect_fault) {
             msg = ok_msg;
-        } else if (fault == NO_FAULT) {
-            msg = no_fault_msg;
         } else {
-            msg = unknown_fault_msg;
+            msg = early_fault_msg;
         }
         write(1, msg, strlen(msg));
         _exit(0);

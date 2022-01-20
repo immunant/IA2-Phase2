62d2cfc525213e4e9f9cc5994967de9e31b08a79
Ayrton Munoz <adm@immunant.com>
2022-01-20 13:46:28 -0500

    Add CHECK_VIOLATION to configure sighandler to expect a fault at mpk violations
    This adds the CHECK_VIOLATION macro to configure the test signal handler to
    expect a segfault when evaluating expressions that cause mpk violations. If a
    test faults before the mpk violation or fails to fault when evaluating the
    expression, the signal handler prints different messages.

--- header-rewriter/tests/should_segfault/print_secret.c
+++ header-rewriter/tests/should_segfault/print_secret.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "print_secret.h"
-#include "untrusted_segfault_handler.h"
+#include "test_fault_handler.h"
+
+static bool early_fault = false;
+
+// Trigger a fault earlier than expected to test that CHECK_VIOLATION prints a
+// different message than in the mpk violation case.
+void do_early_fault() {
+    early_fault = true;
+}
 
 void print_secret() {
-    printf("UNTRUSTED: the secret is %x\n", secret);
+    if (early_fault) {
+        raise(SIGSEGV);
+    }
+    printf("UNTRUSTED: the secret is %x\n", CHECK_VIOLATION(secret));
 }

8147d9201541270be8e3ddc29564118ca554e12c
Frances Wingerter <fw@immunant.com>
2022-02-02 19:22:40 -0500

    provide placeholder definitions of needed runtime values for stack switching
    this includes:
     - untrusted stack (should probably be defined in INIT_COMPARTMENT macro)
     - trusted stack pointer (should be in trusted TLS)
     - untrusted stack pointer (we may not really need this until we support reentrancy)
     - dummy defn of scrub_registers (should be exposed from Rust lib but is not)

--- header-rewriter/HeaderRewriter.cpp
+++ header-rewriter/HeaderRewriter.cpp
@@ -546,6 +546,10 @@ int main(int argc, const char **argv) {
   }
 
   wrapper_out << "#define IA2_WRAPPER\n";
+  wrapper_out << "char untrusted_stack[8192];\n";
+  wrapper_out << "void* ia2_untrusted_stackptr = &untrusted_stack[4096];\n";
+  wrapper_out << "void* ia2_trusted_stackptr;\n";
+  wrapper_out << "void __libia2_scrub_registers(void) {}\n";
   syms_out << "IA2 {\n"
            << "  global:\n";
 

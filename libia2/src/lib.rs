32ff5612eb6baf9d78084eb1410939ea561e1202
Ayrton Munoz <adm@immunant.com>
2022-02-04 12:36:21 -0500

    Update libia2 comments and remove unnecessary pkey init flag from Phase 1

--- libia2/src/lib.rs
+++ libia2/src/lib.rs
@@ -1,30 +1,32 @@
 #![feature(linkage)]
 
 use core::mem::size_of;
-use core::cell::Cell;
 use core::ffi::c_void;
 use core::sync::atomic::{AtomicI32, Ordering};
 
-// Values for the 'trusted compartment pkey' field below.
+// Values for the 'pkeys' field in IA2InitDataSection below.
 // -2 == uninitialized
 // -1 == unsupported
 //  0 == ERROR (this would be the default untrusted pkey)
 // >0 == trusted key
 pub const PKEY_UNINITIALIZED: i32 = -2;
+
+// On linux protection key 0 (the two LSBits of PKRU) is the default for
+// anything not covered by pkey_mprotect so we only have 15 keys to work with.
 const NUM_KEYS: usize = 15;
 
 type CompartmentKeys = [AtomicI32; NUM_KEYS];
 
-// The ia2_init_data section contains data which must be written during
-// library initialization, and then become readable to both trusted and
-// untrusted code, but not writeable from untrusted.  It may (but doesn't
-// currently) remain writeable from trusted.
+// The ia2_init_data section contains data which is written to during
+// compartment initialization and then becomes read-only. Loading a library that
+// initializes a new compartment will make this writeable again, but it must be
+// made read-only before returning to the compartment.
+#[repr(C)]
 #[repr(align(4096))]
 pub struct IA2InitDataSection {
-    // Trusted Compartment PKEY. This is the value of the pkey for the trusted
-    // compartment if initialized or the initialization status if not.  See
-    // values defined above.
-    pub pkeys: CompartmentKeys,
+    // These are the values of the compartment protection keys or
+    // `PKEY_UNINITIALIZED` if not in use.
+    pkeys: CompartmentKeys,
     _padding: [u8; PAGE_SIZE - size_of::<CompartmentKeys>()],
 }
 
@@ -37,81 +39,61 @@ pub static IA2_INIT_DATA: IA2InitDataSection = IA2InitDataSection {
     _padding: [0; PAGE_SIZE - size_of::<CompartmentKeys>()],
 };
 
-thread_local!(static THREAD_HEAP_PKEY_FLAG: Cell<[bool; NUM_KEYS]> = Cell::new([false; NUM_KEYS]));
-
+/// Arguments passed to dl_iterate_phdr while searching for the segments to
+/// initialize for a given compartment key.
 #[repr(C)]
-struct PhdrArgs {
+struct PhdrSearchArgs {
+    /// The index of the compartment key to use when the segments are found
     pkey_idx: usize,
+    /// The address to search for while iterating through segments
     address: *const libc::c_void,
 }
 
-// TODO: Remove the heap arguments to initialize_heap_pkey. This is just a stopgap until we figure
-// out what to do for the allocator
-/// Allocates a protection key and calls `pkey_mprotect` on all pages in the trusted compartment and
-/// the pages defined by `heap_start` and `heap_len`. The input heap is ignored if either argument is
-/// zero.
+/// Allocates a protection key and stores it in the `pkey_idx` slot in
+/// IA2_INIT_DATA. Also calls `pkey_mprotect` on all pages in the compartment
+/// containing `address`. This is only used internally by INIT_COMPARTMENT in ia2.h.
 #[no_mangle]
 pub extern "C" fn initialize_compartment(pkey_idx: usize, address: *const libc::c_void) {
-    THREAD_HEAP_PKEY_FLAG.with(|pkey_flags| {
-        let mut flags = pkey_flags.get();
-        if !flags[pkey_idx] {
-            flags[pkey_idx] = true;
-            pkey_flags.set(flags);
+    let pkey = IA2_INIT_DATA.pkeys[pkey_idx].load(Ordering::Relaxed);
 
-            // Note: We carefully arranged above that the contents of this section
-            // are one object of exactly page size which is page aligned.  Assert
-            // that we succeeded, then mprotect.
-            let (start, size) = unsafe {
-                let start = __start_ia2_init_data as usize;
-                let stop = __stop_ia2_init_data as usize;
-                assert!(start % PAGE_SIZE == 0 && stop % PAGE_SIZE == 0);
-                let size = stop - start;
-                assert!(size % PAGE_SIZE == 0);
-                (start, size)
-            };
+    if pkey == PKEY_UNINITIALIZED {
+        // We carefully arranged above that the contents of this section are
+        // one object of exactly page size which is page aligned.  Assert
+        // that we succeeded, then mprotect.
+        let (start, size) = unsafe {
+            let start = __start_ia2_init_data as usize;
+            let stop = __stop_ia2_init_data as usize;
+            assert!(start % PAGE_SIZE == 0 && stop % PAGE_SIZE == 0);
+            let size = stop - start;
+            assert!(size % PAGE_SIZE == 0);
+            (start, size)
+        };
 
-            // Change the permissions on the memory containing the ia2_init_data section.
-            unsafe {
-                let res = libc::mprotect(start as *mut c_void, size, libc::PROT_READ | libc::PROT_WRITE);
-                assert!(res == 0);
-            }
-
-            let mut pkey = IA2_INIT_DATA.pkeys[pkey_idx].load(Ordering::Relaxed);
-            if pkey == PKEY_UNINITIALIZED {
-                pkey = unsafe { libc::syscall(libc::SYS_pkey_alloc, 0u32, 0u32) as i32 };
-                let result = IA2_INIT_DATA.pkeys[pkey_idx].compare_exchange(
-                    PKEY_UNINITIALIZED,
-                    pkey,
-                    Ordering::Relaxed,
-                    Ordering::Relaxed,
-                );
-                if let Err(other_pkey) = result {
-                    assert!(other_pkey != PKEY_UNINITIALIZED);
-                    // Another thread beat us to it and already
-                    // allocated a key, so release ours
-                    unsafe {
-                        libc::syscall(libc::SYS_pkey_free, pkey);
-                    }
-                }
-            }
-
-            unsafe {
-                let mut args = PhdrArgs {
-                    pkey_idx,
-                    address,
-                };
-                // Iterate through all ELF segments and assign
-                // protection keys to ours
-                libc::dl_iterate_phdr(Some(phdr_callback), &mut args as *mut PhdrArgs as *mut _);
-
-                // Now that all segmentss are setup correctly and we've done
-                // the init (above) revoke the write protections on
-                // the ia2_init_data section.
-                let res = libc::mprotect(start as *mut c_void, size, libc::PROT_READ);
-                assert!(res == 0);
-            }
+        // Change the permissions on the memory containing the ia2_init_data section.
+        unsafe {
+            let res = libc::mprotect(start as *mut c_void, size, libc::PROT_READ | libc::PROT_WRITE);
+            assert!(res == 0);
         }
-    })
+
+        let new_pkey = unsafe { libc::syscall(libc::SYS_pkey_alloc, 0u32, 0u32) as i32 };
+        IA2_INIT_DATA.pkeys[pkey_idx].store(new_pkey, Ordering::Relaxed);
+
+        let mut args = PhdrSearchArgs {
+            pkey_idx,
+            address,
+        };
+        // Iterate through all ELF segments and assign
+        // protection keys to ours
+        unsafe {
+            libc::dl_iterate_phdr(Some(phdr_callback), &mut args as *mut PhdrSearchArgs as *mut _);
+
+            // Now that all segmentss are setup correctly and we've done
+            // the init (above) revoke the write protections on
+            // the ia2_init_data section.
+            let res = libc::mprotect(start as *mut c_void, size, libc::PROT_READ);
+            assert!(res == 0);
+        }
+    }
 }
 
 extern "C" {
@@ -136,11 +118,11 @@ unsafe extern "C" fn phdr_callback(
 ) -> libc::c_int {
     let info = &*info;
 
-    let phdr_args = &*(data as *const PhdrArgs);
+    let phdr_args = &*(data as *const PhdrSearchArgs);
     let pkey_idx = phdr_args.pkey_idx;
     let address = phdr_args.address;
-    // Use the address of `_start` to determine which program headers belong to the trusted
-    // compartment
+    // Use the address in PhdrSearchArgs to determine which program headers
+    // belong to the compartment being initialized.
     let is_trusted_compartment = (0..info.dlpi_phnum).any(|i| {
         let phdr = &*info.dlpi_phdr.add(i.into());
         if phdr.p_type == libc::PT_LOAD {

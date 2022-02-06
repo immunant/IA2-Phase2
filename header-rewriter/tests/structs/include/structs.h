4f0dee604726f31ea6b59b8ff99c610b0c43ced9
Frances Wingerter <fw@immunant.com>
2022-02-05 21:43:16 -0500

    add 'structs' test case to exercise call asm generator

---
+++ header-rewriter/tests/structs/include/structs.h
@@ -0,0 +1,172 @@
+/*
+RUN: cp %s %t.h
+RUN: ia2-header-rewriter %T/wrapper.c %t.h -- -I%resource_dir
+RUN: cat %t.h | sed 's/^.*CHECK.*$//' | FileCheck %s
+RUN: %binary_dir/tests/structs/structs-main | diff %binary_dir/tests/structs/structs.out -
+*/
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+struct s1 {
+	int i1;
+	int i2;
+};
+
+struct s2 {
+	float f1;
+};
+
+struct s3 {
+    int i1;
+    char c1;
+    unsigned int u1;
+};
+
+struct s4 {
+	float f1;
+	float f2;
+};
+
+struct s5 {
+	int i1;
+	void* p1;
+	char ac1[3];
+	void* p2;
+	float f1;
+};
+
+struct __attribute__((packed)) s6 {
+	int i1;
+	char c1;
+	int i2;
+	char c2;
+	size_t z1;
+};
+
+struct s7 {
+	bool b1 : 1;
+	char c1 : 7;
+	unsigned int u1;
+};
+
+struct s8 {
+	bool b1 : 1;
+	char c1 : 5;
+	size_t z1 : 54;
+	char c2 : 7;
+	size_t z2 : 61;
+	size_t z3;
+};
+
+struct s9 {
+	__int128 i1 : 54;
+	__int128 i2 : 13;
+	__int128 i3 : 61;
+};
+
+struct s10 {
+	__int128 i1 : 54;
+	__int128 i2 : 13;
+	__int128 i3 : 61;
+	size_t z1;
+};
+
+struct s11 {
+	size_t az1[9];
+};
+
+struct s12 {
+	char ac1[9];
+};
+
+// CHECK: IA2_WRAP_FUNCTION(get_s1);
+struct s1 get_s1(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s1);
+int cksum_s1(struct s1);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s2);
+struct s2 get_s2(void);
+
+// CHECK: IA2_WRAP_FUNCTION(extract_s2);
+float extract_s2(struct s2);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s3);
+struct s3 get_s3(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s3);
+int cksum_s3(struct s3);
+
+// CHECK: IA2_WRAP_FUNCTION(inc_s3);
+struct s3 inc_s3(struct s3);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s4);
+struct s4 get_s4(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s4);
+float cksum_s4(struct s4);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s5);
+struct s5 get_s5(void);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s5_int);
+struct s5 get_s5_int(int);
+
+// CHECK: IA2_WRAP_FUNCTION(print_s5);
+void print_s5(struct s5 );
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s5_f);
+float cksum_s5_f(struct s5);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s5_z);
+size_t cksum_s5_z(struct s5);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s6);
+struct s6 get_s6(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s6);
+size_t cksum_s6(struct s6);
+
+// CHECK: IA2_WRAP_FUNCTION(inc_s6);
+struct s6 inc_s6(struct s6);
+
+// CHECK: IA2_WRAP_FUNCTION(mix_s6);
+struct s6 mix_s6(struct s6 s1, struct s6 s2);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s7);
+struct s7 get_s7(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s7);
+unsigned int cksum_s7(struct s7);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s8);
+struct s8 get_s8(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s8);
+size_t cksum_s8(struct s8);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s9);
+struct s9 get_s9(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s9);
+__int128 cksum_s9(struct s9);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s10);
+struct s10 get_s10(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s10);
+__int128 cksum_s10(struct s10);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s11);
+struct s11 get_s11(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s11);
+unsigned int cksum_s11(struct s11);
+
+// CHECK: IA2_WRAP_FUNCTION(get_s12);
+struct s12 get_s12(void);
+
+// CHECK: IA2_WRAP_FUNCTION(cksum_s12);
+unsigned int cksum_s12(struct s12);
---
+++ header-rewriter/tests/structs/structs.c
@@ -0,0 +1,218 @@
+#include "structs.h"
+#include <stdio.h>
+
+struct s1 get_s1(void) {
+	struct s1 s = {
+		3,
+		96,
+	};
+	return s;
+}
+
+int cksum_s1(struct s1 s) {
+	return s.i1 + s.i2;
+}
+
+struct s2 get_s2(void) {
+	struct s2 s = {
+		3.14,
+	};
+	return s;
+}
+
+float extract_s2(struct s2 s) {
+	return s.f1;
+}
+
+struct s3 get_s3(void) {
+	struct s3 s = {
+		.i1 = 50,
+		.c1 = 65,
+		.u1 = 65379,
+	};
+	return s;
+}
+
+int cksum_s3(struct s3 s) {
+	return s.i1 + s.c1 + s.u1;
+}
+
+struct s3 inc_s3(struct s3 s) {
+	s.i1++;
+	s.c1++;
+	s.u1++;
+	return s;
+}
+
+struct s4 get_s4(void) {
+	struct s4 s = {
+		.f1 = 3.14,
+		.f2 = 0.0016,
+	};
+	return s;
+}
+
+float cksum_s4(struct s4 s) {
+	return s.f1 + s.f2;
+}
+
+struct s5 get_s5(void) {
+	struct s5 s = {
+		.i1 = 31,
+		.p1 = (void*)0x4000030,
+		.ac1 = {1, 2, 3},
+		.p2 = (void*)0xffffbeef,
+		.f1 = 98.7,
+	};
+	return s;
+}
+
+struct s5 get_s5_int(int i1) {
+	struct s5 s = {
+		.i1 = i1,
+		.p1 = (void*)0x4000030,
+		.ac1 = {1, 2, 3},
+		.p2 = (void*)0xffffbeef,
+		.f1 = 98.7,
+	};
+	return s;
+}
+
+void print_s5(struct s5 s) {
+	printf("s5 { %d %p [%d,%d,%d] %p %.4f }\n",
+		s.i1, s.p1, s.ac1[0], s.ac1[1], s.ac1[2], s.p2, s.f1);
+}
+
+float cksum_s5_f(struct s5 s) {
+	return s.f1;
+}
+size_t cksum_s5_z(struct s5 s) {
+	return s.i1 + (size_t)s.p1 + s.ac1[0] + s.ac1[1] + s.ac1[2] + (size_t)s.p2;
+}
+
+struct s6 get_s6(void) {
+	struct s6 s = {
+		.i1 = 8043,
+		.c1 = 4,
+		.i2 = 695465,
+		.c2 = 100,
+		.z1 = 0x9400000,
+	};
+	return s;
+}
+
+size_t cksum_s6(struct s6 s) {
+	return s.i1 + s.c1 + s.i2 + s.c2 + s.z1;
+}
+
+struct s6 inc_s6(struct s6 s) {
+	s.i1++;
+	s.c1++;
+	s.i2++;
+	s.c2++;
+	s.z1++;
+	return s;
+}
+
+struct s6 mix_s6(struct s6 s1, struct s6 s2) {
+	s1.i1 += s2.i1 / 2;
+	s1.c1 += s2.c1 / 2;
+	s1.i2 += s2.i2 / 2;
+	s1.c2 += s2.c2 / 2;
+	s1.z1 += s2.z1 / 2;
+	return s1;
+}
+
+struct s7 get_s7(void) {
+	struct s7 s = {
+		.b1 = true,
+		.c1 = 55,
+		.u1 = 454000,
+	};
+	return s;
+}
+
+unsigned int cksum_s7(struct s7 s) {
+	return s.b1 + s.c1 + s.u1;
+}
+
+struct s8 get_s8(void) {
+	struct s8 s = {
+		.b1 = true,
+		.c1 = 15,
+		.z1 = 454000,
+		.c2 = 13,
+		.z2 = 9685031,
+		.z3 = 62121255,
+	};
+	return s;
+}
+
+size_t cksum_s8(struct s8 s) {
+	return s.b1 + s.c1 + s.z1 + s.c2 + s.z2 + s.z3;
+}
+
+struct s9 get_s9(void) {
+	struct s9 s = {
+		.i1 = 96506328,
+		.i2 = 1846,
+		.i3 = 3254,
+	};
+	return s;
+}
+
+__int128 cksum_s9(struct s9 s) {
+	return s.i1 + s.i2 + s.i3;
+}
+
+struct s10 get_s10(void) {
+	struct s10 s = {
+		.i1 = 96506328,
+		.i2 = 1846,
+		.i3 = 3254,
+		.z1 = 6853785,
+	};
+	return s;
+}
+
+__int128 cksum_s10(struct s10 s) {
+	return s.i1 + s.i2 + s.i3 + s.z1;
+}
+
+struct s11 get_s11(void) {
+	struct s11 s = {
+		.az1 = {1, 2, 3, 4, 5, 6, 7, 8, 9},
+	};
+	return s;
+}
+
+unsigned int cksum_s11(struct s11 s) {
+	return 9 * s.az1[0]
+		+ 8 * s.az1[1]
+		+ 7 * s.az1[2]
+		+ 6 * s.az1[3]
+		+ 5 * s.az1[4]
+		+ 4 * s.az1[5]
+		+ 3 * s.az1[6]
+		+ 2 * s.az1[7]
+		+ 1 * s.az1[8];
+}
+
+struct s12 get_s12(void) {
+	struct s12 s = {
+		.ac1 = {1, 2, 3, 4, 5, 6, 7, 8, 9},
+	};
+	return s;
+}
+
+unsigned int cksum_s12(struct s12 s) {
+	return 9 * s.ac1[0]
+		+ 8 * s.ac1[1]
+		+ 7 * s.ac1[2]
+		+ 6 * s.ac1[3]
+		+ 5 * s.ac1[4]
+		+ 4 * s.ac1[5]
+		+ 3 * s.ac1[6]
+		+ 2 * s.ac1[7]
+		+ 1 * s.ac1[8];
+}

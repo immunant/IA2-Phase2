e4645e6b1fa0399b874e63625ef7e318498e643a
Ayrton Munoz <adm@immunant.com>
2022-02-16 12:23:50 -0500

    Add align attribute to untrusted_stack

--- header-rewriter/HeaderRewriter.cpp
+++ header-rewriter/HeaderRewriter.cpp
@@ -568,7 +568,7 @@ int main(int argc, const char **argv) {
   // Add dummy definitions of the untrusted stack, trusted TLS storage
   // for stack pointers, and the __libia2_scrub_registers function
   wrapper_out
-      << "static char untrusted_stack[8 * 1024 * 1024];\n"
+      << "static char untrusted_stack[8 * 1024 * 1024] __attribute__((aligned(16)));\n"
       << "static void* ia2_untrusted_stackptr = &untrusted_stack[4 * 1024 * 1024];\n"
       << "static void* ia2_trusted_stackptr;\n"
       << "void __libia2_scrub_registers(void) {}\n";

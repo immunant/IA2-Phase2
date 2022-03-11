6ff28be0da7982db788a0b676fadb2d7539bbbdf
Ayrton Munoz <adm@immunant.com>
2022-03-11 02:39:56 -0500

    Move PKRU code to GenCallAsm.cpp

--- header-rewriter/GenCallAsm.h
+++ header-rewriter/GenCallAsm.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "CAbi.h"
 
-/* The kind of call to generate a wrapper */
+/* The kind of call to generate a wrapper for */
 enum class WrapperKind {
-    // Direct call to another compartment
-    Direct,
-    // Indirect call through a pointer received from another compartment
-    IndirectFromTrusted,
-    // Indirect call through a pointer sent to another compartment
-    IndirectFromUntrusted,
+  // Direct call to another compartment
+  Direct,
+  // Indirect call through a pointer received from another compartment
+  IndirectFromTrusted,
+  // Indirect call through a pointer sent to another compartment
+  IndirectFromUntrusted,
 };
 
+// Generates a wrapper for a function with the given signature and name.
+// WrapperKind determines the type of call which may affect the order of
+// operations and the layout of the wrapper stack frame. compartment_pkey is
+// the callee's protection key. The caller's protection key is always a macro
+// definition or parameter which is determined by the type of call.
 std::string emit_asm_wrapper(const CAbiSignature &sig, const std::string &name,
-                             WrapperKind kind);
+                             WrapperKind kind,
+                             const std::string &compartment_pkey);

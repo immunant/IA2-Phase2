4a2a2833631dec1ee75482a1ef78351d3fe536b9
Ayrton Munoz <adm@immunant.com>
2022-03-29 12:52:36 -0400

    Change INCLUDE_DIR in define_ia2_wrapper to INPUT_DIR and add INCLUDE_DIRS

--- cmake/define-ia2-wrapper.cmake
+++ cmake/define-ia2-wrapper.cmake
@@ -27,7 +27,7 @@ execute_process(COMMAND ${CMAKE_C_COMPILER} -print-file-name=include-fixed
 #               ${TEST_NAME}-main.
 # OUTPUT_HEADER - Header for IA2's type-specific function pointer macros.
 #                 Defaults to ${WRAPPED_LIB}_fn_ptr_ia2.h.
-# INCLUDE_DIR - Added to search path in rewriter invocation. Defaults to
+# INPUT_DIR - Added to search path in rewriter invocation. Defaults to
 #               SRC_DIR/include.
 # OUTPUT_DIR - Output directory relative to BIN_DIR to create and use for
 #              rewritten headers. Defaults to BIN_DIR.
@@ -38,9 +38,9 @@ execute_process(COMMAND ${CMAKE_C_COMPILER} -print-file-name=include-fixed
 function(define_ia2_wrapper)
     # Parse options
     set(options USE_SYSTEM_HEADERS WRAP_MAIN)
-    set(oneValueArgs WRAPPER WRAPPED_LIB OUTPUT_HEADER INCLUDE_DIR OUTPUT_DIR
+    set(oneValueArgs WRAPPER WRAPPED_LIB OUTPUT_HEADER OUTPUT_DIR INPUT_DIR
         COMPARTMENT_PKEY CALLER_PKEY)
-    set(multiValueArgs HEADERS PRIVATE_HEADERS SHARED_HEADERS)
+    set(multiValueArgs HEADERS PRIVATE_HEADERS SHARED_HEADERS INCLUDE_DIRS)
     cmake_parse_arguments(DEFINE_IA2_WRAPPER "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN} )
 
@@ -67,15 +67,15 @@ function(define_ia2_wrapper)
     else()
         set(OUTPUT_HEADER "${WRAPPED_LIB}_fn_ptr_ia2.h")
     endif()
-    if(DEFINED DEFINE_IA2_WRAPPER_INCLUDE_DIR)
-        set(INCLUDE_DIR ${DEFINE_IA2_WRAPPER_INCLUDE_DIR})
+    if(DEFINED DEFINE_IA2_WRAPPER_INPUT_DIR)
+        set(INPUT_DIR ${DEFINE_IA2_WRAPPER_INPUT_DIR})
     else()
         # Use system path for system libs and `source/include` for in-tree libs
         if(${DEFINE_IA2_WRAPPER_USE_SYSTEM_HEADERS})
             pkg_check_modules(LIB REQUIRED lib${WRAPPED_LIB})
-            set(INCLUDE_DIR ${LIB_INCLUDEDIR})
+            set(INPUT_DIR ${LIB_INCLUDEDIR})
         else()
-            set(INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/include)
+            set(INPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/include)
         endif()
     endif()
     if(DEFINED DEFINE_IA2_WRAPPER_COMPARTMENT_PKEY)
@@ -97,9 +97,13 @@ function(define_ia2_wrapper)
     if(DEFINED DEFINE_IA2_WRAPPER_SHARED_HEADERS)
         set(SHARED_HEADERS "--shared-headers=${DEFINE_IA2_WRAPPER_SHARED_HEADERS}")
     endif()
+    if(DEFINED DEFINE_IA2_WRAPPER_INCLUDE_DIRS)
+        set(INCLUDE_DIRS ${DEFINE_IA2_WRAPPER_INCLUDE_DIRS})
+        list(TRANSFORM INCLUDE_DIRS PREPEND "-I")
+    endif()
 
     # Collect headers
-    set(ORIGINAL_HEADER_DIR ${INCLUDE_DIR})
+    set(ORIGINAL_HEADER_DIR ${INPUT_DIR})
     set(REWRITTEN_HEADER_DIR ${OUTPUT_DIR})
 
     if(${DEFINE_IA2_WRAPPER_USE_SYSTEM_HEADERS})
@@ -109,7 +113,7 @@ function(define_ia2_wrapper)
         foreach(SYSTEM_HEADER ${HEADERS})
             # Make path absolute
             if(NOT IS_ABSOLUTE ${SYSTEM_HEADER})
-                set(SYSTEM_HEADER ${INCLUDE_DIR}/${SYSTEM_HEADER})
+                set(SYSTEM_HEADER ${INPUT_DIR}/${SYSTEM_HEADER})
             endif()
 
             # Copy the header to build dir under `system` subdir
@@ -162,6 +166,7 @@ function(define_ia2_wrapper)
           --
           -fgnuc-version=6
           -I ${REWRITTEN_HEADER_DIR}
+          ${INCLUDE_DIRS}
           -isystem ${C_SYSTEM_INCLUDE}
           -isystem ${C_SYSTEM_INCLUDE_FIXED}
         DEPENDS ${HEADER_SRCS} ia2-header-rewriter
@@ -188,7 +193,7 @@ function(define_ia2_wrapper)
 
     # Add IA2 and wrapper include dirs
     target_include_directories(${WRAPPER_TARGET}
-        BEFORE PUBLIC ${IA2_INCLUDE_DIR}
+        BEFORE PUBLIC ${IA2_INPUT_DIR}
         INTERFACE ${REWRITTEN_HEADER_DIR}
     )
 

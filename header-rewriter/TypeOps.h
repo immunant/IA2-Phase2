ca1319f0cdb2029049b3107b8a92e8757ec690f0
Frances Wingerter <fw@immunant.com>
2022-04-18 18:58:48 -0400

    split Type operations into their own file
    monomorphize replace_type_placeholder on the way

---
+++ header-rewriter/TypeOps.h
@@ -0,0 +1,12 @@
+#include "clang/AST/AST.h"
+#include <string>
+
+extern const std::string kTypePlaceHolder;
+extern const std::string kFnPtrTypePrefix;
+
+std::string type_string(const clang::QualType &ty);
+std::string type_string_with_placeholder(const clang::QualType &ty);
+std::string replace_type_placeholder(std::string s, const std::string &with);
+
+std::string mangle_type(clang::ASTContext &ctx, const clang::QualType &ty);
+std::string mangle_name(const clang::FunctionDecl *decl);

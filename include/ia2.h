b515930fa494b824bfa8401bd3d46dbc8435bc09
Ayrton Munoz <adm@immunant.com>
2022-04-18 15:38:11 -0400

    Remove function definitions from ia2.h
    Defining functions in ia2.h means that this header can't be included by two
    files that are linked together. At some point we should move these functions to
    a proper source file, but that's not too important for now.

--- include/ia2.h
+++ include/ia2.h
@@ -18,7 +18,6 @@
 // flexible.
 #define UNIQUE_STR(s) s "_line_" XSTR(__LINE__)
 
-#define INIT_RUNTIME(n) _INIT_RUNTIME(n)
 #ifdef LIBIA2_INSECURE
 #define INIT_COMPARTMENT(n) DECLARE_PADDING_SECTIONS
 #define IA2_WRPKRU
@@ -90,20 +89,6 @@ asm(".macro mov_mixed_pkru_eax pkey0, pkey1\n"
   NEW_SECTION(".eh_frame_padding");                                            \
   NEW_SECTION(".bss_padding");
 
-// Ensure that all required pkeys are allocated.
-void ensure_pkeys_allocated(int *n_to_alloc) {
-  if (*n_to_alloc != 0) {
-    for (int pkey = 1; pkey <= *n_to_alloc; pkey++) {
-      int allocated = pkey_alloc(0, 0);
-      if (allocated != pkey) {
-        printf("Failed to allocate protection keys in the expected order\n");
-        exit(-1);
-      }
-    }
-    *n_to_alloc = 0;
-  }
-}
-
 // Initializes a compartment with protection key `n` when the ELF invoking this
 // macro is loaded. This must only be called once for each key. The compartment
 // includes all segments in the ELF except the `ia2_shared_data` section, if one
@@ -121,8 +106,6 @@ void ensure_pkeys_allocated(int *n_to_alloc) {
   }
 
 // TODO: Find a better way to compute these offsets for ia2_untrusted_stackptr.
-// It may be easier to make our pkey numbers match linux's where the untrusted
-// pkey is 0 and everything else increases by 1.
 #define STACK(n) _STACK(n)
 #define _STACK(n) STACK_##n
 #define STACK_0 "0"
@@ -144,42 +127,59 @@ void ensure_pkeys_allocated(int *n_to_alloc) {
 
 #define STACK_SIZE (4 * 1024 * 1024)
 
-// Allocate the 'i'th stack with appropriate permissions and set its pkey.
-char *allocate_stack(int i) {
-  char *stack = (char *)mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
-                             MAP_PRIVATE | MAP_ANON, -1, 0);
-  if (stack == MAP_FAILED) {
-    printf("Failed to allocate stack %d (%d)\n", i, errno);
-    exit(-1);
-  }
-#ifndef LIBIA2_INSECURE
-  int res = pkey_mprotect(stack, STACK_SIZE, PROT_READ | PROT_WRITE, i);
-  if (res == -1) {
-    printf("Failed to mprotect stack %d (%d)\n", i, errno);
-  }
-#endif
-  // Each stack frame start + 8 is initially 16-byte aligned.
-  stack += STACK_SIZE - 8;
-  return stack;
-}
-
-// Sets up stacks and defines how many protection keys need to be allocated
-#ifndef LIBIA2_INSECURE
-#define _INIT_RUNTIME(n)                                                       \
+#ifdef LIBIA2_INSECURE
+#define INIT_RUNTIME(n)                                                        \
+  int ia2_n_pkeys_to_alloc = 0;                                                \
+  void protect_stack(int i, char *stack) {}                                    \
+  INIT_RUNTIME_COMMON(n)
+#else
+#define INIT_RUNTIME(n)                                                        \
   int ia2_n_pkeys_to_alloc = n;                                                \
+  /* Protect a stack with the ith pkey. */                                     \
+  void protect_stack(int i, char *stack) {                                     \
+    if (!stack) {                                                              \
+      exit(-1);                                                                \
+    }                                                                          \
+    int res = pkey_mprotect(stack, STACK_SIZE, PROT_READ | PROT_WRITE, i);     \
+    if (res == -1) {                                                           \
+      printf("Failed to mprotect stack %d (%d)\n", i, errno);                  \
+    }                                                                          \
+  }                                                                            \
+  INIT_RUNTIME_COMMON(n)
+#endif
+
+#define INIT_RUNTIME_COMMON(n)                                                 \
+  /* Allocate a fixed-size stack. */                                           \
+  char *allocate_stack(int i) {                                                \
+    char *stack = (char *)mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,       \
+                               MAP_PRIVATE | MAP_ANON, -1, 0);                 \
+    if (stack == MAP_FAILED) {                                                 \
+      printf("Failed to allocate stack %d (%d)\n", i, errno);                  \
+      exit(-1);                                                                \
+    }                                                                          \
+    return stack;                                                              \
+  }                                                                            \
+  /* Ensure that all required pkeys are allocated. */                          \
+  void ensure_pkeys_allocated(int *n_to_alloc) {                               \
+    if (*n_to_alloc != 0) {                                                    \
+      for (int pkey = 1; pkey <= *n_to_alloc; pkey++) {                        \
+        int allocated = pkey_alloc(0, 0);                                      \
+        if (allocated != pkey) {                                               \
+          printf(                                                              \
+              "Failed to allocate protection keys in the expected order\n");   \
+          exit(-1);                                                            \
+        }                                                                      \
+      }                                                                        \
+      *n_to_alloc = 0;                                                         \
+    }                                                                          \
+  }                                                                            \
   char *ia2_stackptrs[n + 1] IA2_SHARED_DATA;                                  \
   __attribute__((constructor)) static void init_stacks() {                     \
     ensure_pkeys_allocated(&ia2_n_pkeys_to_alloc);                             \
     for (int i = 0; i < n + 1; i++) {                                          \
-      ia2_stackptrs[i] = allocate_stack(i);                                    \
+      char *stack_start = allocate_stack(i);                                   \
+      protect_stack(i, stack_start);                                           \
+      /* Each stack frame start + 8 is initially 16-byte aligned. */           \
+      ia2_stackptrs[i] = stack_start + STACK_SIZE - 8;                         \
     }                                                                          \
   }
-#else
-#define _INIT_RUNTIME(n)                                                       \
-  char *ia2_stackptrs[n + 1];                                                  \
-  __attribute__((constructor)) static void init_stacks() {                     \
-    for (int i = 0; i < n + 1; i++) {                                          \
-      ia2_stackptrs[i] = allocate_stack(i);                                    \
-    }                                                                          \
-  }
-#endif

47600a3ad1b7cb9b44aeca484826d407dc2ac9d1
Ayrton Munoz <adm@immunant.com>
2022-04-25 23:11:14 -0400

    Make names for manually defined direct call wrappers unique
    Closes #116. I ended up only having to modify the manually defined direct call
    wrappers. Indirect call wrappers have names based off of the stringified
    function pointer expression so name collisions isn't as much of an issue. Direct
    call wrappers in the shim libraries currently don't need to have the pkeys
    appended, but this may be an issue when we start dealing with multiple shims
    calling one library.

--- header-rewriter/tests/global_fn_ptr/main.c
+++ header-rewriter/tests/global_fn_ptr/main.c
@@ -14,21 +14,21 @@ IA2_DEFINE_WRAPPER(mul, _ZTSPFjjjE, 1);
 
 // `sum` can't be set to __ia2_add since it's defined in asm so the compiler doesn't know it's a valid initializer
 //static WordFn sum = __ia2_add;
-static WordFn sum = IA2_WRAPPER(add);
-static HalfFn diff = IA2_WRAPPER(sub);
+static WordFn sum = IA2_WRAPPER(add, 1);
+static HalfFn diff = IA2_WRAPPER(sub, 1);
 // The following won't compile since the inner pointer is also type-specific
-//static HalfFn mul = {&__ia2_mul};
+//static HalfFn mul = {&__ia2_mul_0_1};
 
 Op operations[2] IA2_SHARED_DATA = {
     {
         { "add", 4 },
-        {&__ia2_add},
+        IA2_WRAPPER(add, 1),
         { "Adds two u32s", 14 },
         0,
     },
     {
         { "mul", 4 },
-        {&__ia2_mul},
+        IA2_WRAPPER(mul, 1),
         { "Multiply two u32s", 18 },
         0,
     },

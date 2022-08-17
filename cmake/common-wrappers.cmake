98f8eb32d9ca67f3e8b1230cce6229be206b6f4c
Stephen Crane <sjc@immunant.com>
2022-08-16 17:21:58 -0700

    Improve ffmpeg wrappers
    We now include all available and compilable headers under the ffmpeg
    include directories when making a wrapper for these libraries. This
    avoids the need to explicitly list headers, while still skipping headers
    that simply don't work in the current environment (e.g. missing cuda).

--- cmake/common-wrappers.cmake
+++ cmake/common-wrappers.cmake
@@ -1,22 +1,63 @@
-# ffmpeg system library wrappers
-set(AVFORMAT_HEADERS libavformat/avformat.h libavformat/avio.h)
-set(AVUTIL_HEADERS libavutil/dict.h libavutil/version.h libavutil/macros.h
-    libavutil/common.h libavutil/mem.h libavutil/attributes.h libavutil/avutil.h
-    libavutil/rational.h libavutil/mathematics.h libavutil/intfloat.h
-    libavutil/log.h libavutil/pixfmt.h libavutil/error.h)
+include(CheckIncludeFile)
 
+function(gather_ffmpeg_headers)
+  set(outputVar ${ARGV0})
+  set(libName ${ARGV1})
+
+  find_path(headersPath ${libName})
+  file(GLOB headerList ${headersPath}/${libName}/*)
+
+  # Make sure that we can actually compile with each header, and remove the ones
+  # that we can't use in this configuration
+  foreach(header ${headerList})
+    CHECK_INCLUDE_FILE(${header} ${header}-valid)
+    if(NOT ${header}-valid)
+      list(REMOVE_ITEM headerList ${header})
+    endif()
+  endforeach()
+
+  set(${outputVar} ${headerList} PARENT_SCOPE)
+endfunction()
+
+# ffmpeg system library wrappers
+gather_ffmpeg_headers(AVFORMAT_HEADERS libavformat)
 define_ia2_wrapper(
     WRAPPER avformat_wrapper
     WRAPPED_LIB avformat
     HEADERS ${AVFORMAT_HEADERS}
+    OUTPUT_DIR libavformat
     USE_SYSTEM_HEADERS
     CALLER_PKEY 1
 )
+target_include_directories(avformat_wrapper
+  BEFORE PUBLIC ${IA2_INCLUDE_DIR}
+  INTERFACE ${CMAKE_CURRENT_BINARY_DIR}
+)
 
+gather_ffmpeg_headers(AVUTIL_HEADERS libavutil)
 define_ia2_wrapper(
     WRAPPER avutil_wrapper
     WRAPPED_LIB avutil
     HEADERS ${AVUTIL_HEADERS}
+    OUTPUT_DIR libavutil
     USE_SYSTEM_HEADERS
     CALLER_PKEY 1
+)
+target_include_directories(avutil_wrapper
+  BEFORE PUBLIC ${IA2_INCLUDE_DIR}
+  INTERFACE ${CMAKE_CURRENT_BINARY_DIR}
+)
+
+gather_ffmpeg_headers(AVCODEC_HEADERS libavcodec)
+define_ia2_wrapper(
+    WRAPPER avcodec_wrapper
+    WRAPPED_LIB avcodec
+    HEADERS ${AVCODEC_HEADERS}
+    OUTPUT_DIR libavcodec
+    USE_SYSTEM_HEADERS
+    CALLER_PKEY 1
+)
+target_include_directories(avcodec_wrapper
+  BEFORE PUBLIC ${IA2_INCLUDE_DIR}
+  INTERFACE ${CMAKE_CURRENT_BINARY_DIR}
 )

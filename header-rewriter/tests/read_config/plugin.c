f7f7deee276b9a29f320a8882e0beab6bf9d002d
Ayrton <a.munoz3327@gmail.com>
2023-01-25 15:02:22 -0500

    Remove manually added function pointer annotations from tests
    The source rewriter will add a simplified version of these macros so we don't
    need to check in the annotated sources. This also removes the IA2_DEFINE_WRAPPER
    invocations since those call gate wrappers are defined in the .c generated by
    the source rewriter.

--- header-rewriter/tests/read_config/plugin.c
+++ header-rewriter/tests/read_config/plugin.c
@@ -9,32 +9,6 @@ INIT_COMPARTMENT(2);
 // A custom parsing function for a type defined by the plugin
 static void parse_tuple(char *opt, void *out);
 
-// The indirect callsite in main.c drops the target function to pkey 0, but
-// parse_tuple needs access to this compartment (memcpy is called through the
-// PLT) so must wrap it.
-IA2_DEFINE_WRAPPER(parse_tuple, _ZTSPFvPcPvE, 2);
-
-// In this source file, references to parse_str actually point to the wrapper
-// __ia2_parse_str defined in the shim library. __ia2_parse_str expects a caller
-// with pkey 2 (plugin) and calls the real parse_str with pkey 1 (the main
-// binary's). The indirect calls site drops its target to pkey 0 so it can't
-// call __ia2_parse_str directly. Instead we must define another wrapper
-// __ia2_parse_str_0_2 which calls __ia2_parse_str. This manually defined
-// wrapper expects a caller with pkey 0 and calls __ia2_parse_str with pkey 2.
-// Putting it together we have:
-// callsite wrapper (caller 1/target 0) calls
-// __ia2_parse_str_0_2 (caller 0/target 2) which calls
-// __ia2_parse_str (caller 2/target 1).
-
-// TODO: When we switch to `ld --wrap` (#98) we'll be able to reference
-// the real parse_str from here and this will only require two wrappers instead
-// of three.
-IA2_DEFINE_WRAPPER(parse_str, _ZTSPFvPcPvE, 2);
-// See comment on parse_str
-IA2_DEFINE_WRAPPER(parse_u32, _ZTSPFvPcPvE, 2);
-// See comment on parse_str
-IA2_DEFINE_WRAPPER(parse_bool, _ZTSPFvPcPvE, 2);
-
 // IA2_SHARED_DATA makes all values in this struct shared, which includes the
 // `char *` but not the string literals. This means that the other compartment
 // can see where the string literals are but will not be able to read them. This
@@ -43,32 +17,32 @@ static struct cfg_opt opts[6] IA2_SHARED_DATA = {
     {
         "name",
         str,
-        IA2_WRAPPER(parse_str, 2),
+        parse_str,
     },
     {
         "num_options",
         u32,
-        IA2_WRAPPER(parse_u32, 2),
+        parse_u32,
     },
     {
         "debug_mode",
         boolean,
-        IA2_WRAPPER(parse_bool, 2),
+        parse_bool,
     },
     {
         "magic_val",
         other,
-        IA2_WRAPPER(parse_tuple, 2),
+        parse_tuple,
     },
     {
         "some_flag",
         boolean,
-        IA2_WRAPPER(parse_bool, 2),
+        parse_bool,
     },
     {
         "random_seed",
         u32,
-        IA2_WRAPPER(parse_u32, 2),
+        parse_u32,
     },
 };
 
--- header-rewriter/tests/simple1/main.c
+++ header-rewriter/tests/simple1/main.c
@@ -13,7 +13,7 @@ INIT_COMPARTMENT(1);
 // libsimple1 checks if the function pointer is NULL. To initialize this to a
 // function defined in this binary, we'd need to define a wrapper with
 // IA2_DEFINE_WRAPPER with target pkey 1, then use IA2_WRAPPER.
-static HookFn exit_hook_fn = IA2_NULL_FNPTR;
+static HookFn exit_hook_fn = NULL;
 
 HookFn get_exit_hook(void) { return exit_hook_fn; }
 
@@ -42,8 +42,8 @@ int main() {
   // These will be called from untrusted code but may access trusted compartment
   // 0
   struct SimpleCallbacks scb = {
-      .read_cb = IA2_DEFINE_WRAPPER_FN_SCOPE(main_read, _ZTSPFiiE, 1),
-      .write_cb = IA2_DEFINE_WRAPPER_FN_SCOPE(main_write, _ZTSPFviE, 1),
+      .read_cb = main_read,
+      .write_cb = main_write,
   };
 
   struct Simple *s = simple_new(scb);
@@ -55,21 +55,20 @@ int main() {
   srand(time(NULL));
   // These will be called from untrusted code but may access trusted compartment
   // 0
-  IA2_DEFINE_WRAPPER(main_map, _ZTSPFiiE, 1);
-  simple_foreach_v1(s, IA2_WRAPPER_FN_SCOPE(main_map, 1));
+  simple_foreach_v1(s, main_map);
   simple_reset(s);
-  simple_foreach_v2(s, IA2_WRAPPER_FN_SCOPE(main_map, 1));
+  simple_foreach_v2(s, main_map);
   simple_destroy(s);
 
   // We need to check if exit_hook_fn is NULL since IA2_CALL always
   // returns a non-null pointer. Since it's an opaque pointer, we use this macro
   // instead of directly comparing with NULL.
-  if (!IA2_FNPTR_IS_NULL(exit_hook_fn)) {
+  if (!exit_hook_fn) {
     // Creates a wrapper that assumes the caller has pkey 0 and the callee is
     // untrusted since libsimple1 sets the value of exit_hook_fn. If
     // exit_hook_fn were to point to a function defined in this binary, it must
     // be a wrapped function with an untrusted caller and callee with pkey 0.
-    IA2_CALL(exit_hook_fn, _ZTSPFvvE, 1)();
+    exit_hook_fn();
   }
 
   return 0;

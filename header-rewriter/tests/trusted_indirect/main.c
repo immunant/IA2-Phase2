329ec0f82bb9436419c811febd011b9f85d01f9b
Ayrton Munoz <adm@immunant.com>
2023-04-05 14:32:32 -0400

    Add `used` attribute to functions that have their address taken in tests
    See issue #194.

--- header-rewriter/tests/trusted_indirect/main.c
+++ header-rewriter/tests/trusted_indirect/main.c
@@ -25,7 +25,7 @@ uint32_t multiply(uint32_t x, uint32_t y) {
     return x * y;
 }
 
-static uint32_t divide(uint32_t x, uint32_t y) {
+__attribute__((used)) static uint32_t divide(uint32_t x, uint32_t y) {
     return x / y;
 }
 

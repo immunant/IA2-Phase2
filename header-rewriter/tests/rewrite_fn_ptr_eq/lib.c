f84509e311f272c832a55903e3a2cf7ce06ded3b
Ayrton Munoz <adm@immunant.com>
2023-04-06 13:18:25 -0400

    Add LIT invocation for rewrite_fn_ptr_eq/lib.c

--- header-rewriter/tests/rewrite_fn_ptr_eq/lib.c
+++ header-rewriter/tests/rewrite_fn_ptr_eq/lib.c
@@ -1,8 +1,12 @@
+/*
+RUN: cat rewrite_fn_ptr_eq_call_gates_1.ld | FileCheck --check-prefix=LINKARGS %s
+*/
 #include <lib.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <assert.h>
 
+// LINKARGS: --wrap=call_fn
 int call_fn(bin_op fn, int x, int y) {
     assert(fn != NULL);
     int res = fn(x, y);

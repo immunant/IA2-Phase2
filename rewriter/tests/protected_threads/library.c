30fbeace08903922aa3d200c81c907e282f80e13
Ayrton Munoz <adm@immunant.com>
2023-05-26 13:51:03 -0500

    Fix output reference for protected_threads test

--- rewriter/tests/protected_threads/library.c
+++ rewriter/tests/protected_threads/library.c
@@ -1,5 +1,5 @@
 /*
-RUN: cat threads_call_gates_1.ld | FileCheck --check-prefix=LINKARGS %s
+RUN: cat protected_threads_call_gates_1.ld | FileCheck --check-prefix=LINKARGS %s
 */
 #include "library.h"
 #include <ia2.h>
@@ -9,5 +9,5 @@ RUN: cat threads_call_gates_1.ld | FileCheck --check-prefix=LINKARGS %s
 #define IA2_COMPARTMENT 2
 #include <ia2_compartment_init.inc>
 
-void lib_nop(void) {
-}
+// LINKARGS: --wrap=lib_nop
+void lib_nop(void) {}

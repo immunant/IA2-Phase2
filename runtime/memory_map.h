22a0038203ef20f2cbd5401ebfd201a92ff171e5
Frances Wingerter <fw@immunant.com>
2023-08-28 14:51:15 -0400

    runtime: memory-map: change to storing complex state
    also rename remove -> unmap because unmap can actually split regions but definitely does unmap exactly the region specified

--- runtime/memory_map.h
+++ runtime/memory_map.h
@@ -18,20 +18,20 @@ struct range {
 
 struct memory_map *memory_map_new(void);
 
-void memory_map_destroy(struct memory_map *map);
+void memory_map_destroy(struct memory_map *_map);
 
 bool memory_map_all_overlapping_regions_have_pkey(const struct memory_map *map,
                                                   struct range needle,
                                                   uint8_t pkey);
 
-bool memory_map_remove_region(struct memory_map *map, struct range needle);
+bool memory_map_all_overlapping_regions_pkey_mprotected(const struct memory_map *map,
+                                                        struct range needle,
+                                                        bool pkey_mprotected);
 
-bool memory_map_add_region(struct memory_map *map,
-                           struct range range,
-                           uint8_t owner_pkey);
+bool memory_map_unmap_region(struct memory_map *map, struct range needle);
 
-bool memory_map_split_region(struct memory_map *map,
-                             struct range range,
-                             uint8_t owner_pkey);
+bool memory_map_add_region(struct memory_map *map, struct range range, uint8_t owner_pkey);
+
+bool memory_map_split_region(struct memory_map *map, struct range range, uint8_t owner_pkey);
 
 /* clang-format on */

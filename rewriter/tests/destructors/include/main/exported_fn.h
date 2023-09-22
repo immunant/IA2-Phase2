9ad4829fdc789d7f6569b70e8a5b2a902bd0acb1
Stephen Crane <sjc@immunant.com>
2023-09-22 15:27:35 -0700

    Switch to compartment 0 when calling exit(3)
    Destructors are wrapped to run in their respective compartments, but
    transition back to compartment 0 after running. This means that the
    caller stack must be writable in compartment 0. This change wraps
    exit(3) and transitions to compartment 0 and the shared stack before
    running exit() from libc.
    
    Fixes #285

---
+++ rewriter/tests/destructors/include/main/exported_fn.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+void print_message(void);
+
+// This is exported to avoid an implicit decl error when the plugin tries to
+// access it, but it's explicitly not shared to test that an MPK violation
+// occurs.
+extern uint32_t secret;
+
+extern bool debug_mode;
---
+++ rewriter/tests/destructors/include/plugin/plugin.h
@@ -0,0 +1,13 @@
+// No need to execute the program here since the header exported by the main
+// binary does that.
+#pragma once
+#include <stdint.h>
+
+void start_plugin(void);
+
+void exit_from_plugin(void);
+
+// This is exported to avoid an implicit decl error when the main binary tries
+// to access it, but it's explicitly not shared to test that an MPK violation
+// occurs.
+extern uint32_t plugin_secret;

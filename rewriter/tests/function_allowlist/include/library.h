d4449dbaf2c734d61d90f6d9668b966626ef1d25
Stephen Crane <sjc@immunant.com>
2023-09-22 16:10:24 -0700

    Remove function_allowlist test
    We don't have a --function-allowlist anymore, so this is testing nothing.

--- rewriter/tests/function_allowlist/include/library.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// This function does nothing, but should get wrapped
-void foo();
-
-// This function is not in the allowlist so it should not be wrapped
-void defined_in_main();
--- rewriter/tests/function_allowlist/library.c
+++
@@ -1,8 +0,0 @@
-/*
-RUN: cat function_allowlist_call_gates_1.ld | FileCheck --check-prefix=LINKARGS %s
-*/
-#include "library.h"
-#include <stdio.h>
-
-// LINKARGS: --wrap=foo
-void foo() { printf("foo\n"); }

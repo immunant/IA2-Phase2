865c3e59db43779f307504f2db7f7f19d434b17d
Stephen Crane <sjc@immunant.com>
2023-09-28 12:32:00 -0700

    Add shared_memalign to allocator

--- partition-alloc/include/ia2_allocator.h
+++ partition-alloc/include/ia2_allocator.h
@@ -5,3 +5,5 @@ void *shared_malloc(size_t bytes);
 void shared_free(void *ptr);
 void *shared_realloc(void *ptr, size_t size);
 void *shared_calloc(size_t num, size_t size);
+void *shared_memalign(size_t algin, size_t size);
+int shared_posix_memalign(void **res, size_t align, size_t size);

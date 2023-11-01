6f1e229b5fc406efbc24c25a19d9ca4d4de1a0ed
Frances Wingerter <fw@immunant.com>
2023-11-01 17:34:40 -0400

    runtime/track_memory_map: manage memory map internally

--- runtime/start.c
+++ runtime/start.c
@@ -107,10 +107,8 @@ int main(int argc, char **argv) {
   }
 
   int exit_status = 0;
-  struct memory_map *map = memory_map_new();
-  bool success = track_memory_map(pid, map, &exit_status, TRACE_MODE_SECCOMP);
+  bool success = track_memory_map(pid, &exit_status, TRACE_MODE_SECCOMP);
   ptrace(PTRACE_KILL, pid, 0, 0);
-  memory_map_destroy(map);
 
   if (success)
     exit(exit_status);

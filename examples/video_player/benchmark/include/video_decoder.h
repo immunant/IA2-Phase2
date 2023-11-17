91150b5c2b8c9e0f3d68b7e1c11f9e3e6cf6a42f
Stephen Crane <sjc@immunant.com>
2023-11-16 18:16:16 -0800

    Add video decoding benchmark

---
+++ examples/video_player/benchmark/include/video_decoder.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023 Immunant, Inc.
+ */
+#pragma once
+
+#include <libavcodec/avcodec.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+struct video_decoder;
+
+typedef void (*video_decoder_frame_callback_t)(AVFrame*, void*);
+
+struct video_decoder *video_decoder_init(const char *file_data,
+                                         size_t file_size);
+
+void video_decoder_get_info(struct video_decoder *, int *width, int *height,
+                            enum AVPixelFormat *pix_fmt);
+
+bool video_decoder_decode(struct video_decoder *, void *,
+                          video_decoder_frame_callback_t);

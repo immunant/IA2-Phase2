b4fc09b363d97341b9c90430393c1f09daa04f31
Stephen Crane <sjc@immunant.com>
2023-11-16 16:06:13 -0800

    Add microbenchmarks for compartment transitions

---
+++ benchmarks/microbench/microbench_indirect.c
@@ -0,0 +1,43 @@
+#include "b63/register.h"
+#include <b63/b63.h>
+#include <b63/counters/perf_events.h>
+#include <ia2.h>
+
+#include "microbench_lib.h"
+#include "microbench_lib2.h"
+
+INIT_RUNTIME(2);
+#define IA2_COMPARTMENT 1
+#include <ia2_compartment_init.inc>
+
+void (*fn_ptr)(void);
+
+IA2_BEGIN_NO_WRAP
+void (*fn_ptr_unwrapped)(void);
+IA2_END_NO_WRAP
+
+B63_BASELINE(indirect, n) {
+    fn_ptr_unwrapped = no_op_unwrapped;
+    for (size_t i = 0; i < n; ++i) {
+        fn_ptr_unwrapped();
+    }
+}
+
+B63_BENCHMARK(indirect_wrapped, n) {
+    IA2_AS_PTR(fn_ptr) = (void*)no_op_unwrapped;
+    for (size_t i = 0; i < n; ++i) {
+        fn_ptr();
+    }
+}
+
+B63_BENCHMARK(indirect_compartment2, n) {
+    fn_ptr = no_op_compartment2;
+    for (size_t i = 0; i < n; ++i) {
+        IA2_CALL(fn_ptr, 0)();
+    }
+}
+
+int main(int argc, char* argv[]) {
+    B63_RUN(argc, argv);
+    return 0;
+}
---
+++ benchmarks/microbench/microbench_lib_indirect_compartment2.c
@@ -0,0 +1,8 @@
+#include "microbench_lib2.h"
+
+#include <ia2.h>
+
+#define IA2_COMPARTMENT 2
+#include <ia2_compartment_init.inc>
+
+void no_op_compartment2(void) {}

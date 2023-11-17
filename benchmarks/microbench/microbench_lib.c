510b20cd00a20c32633d0eef53a68df52b6bec01
Stephen Crane <sjc@immunant.com>
2023-11-16 16:06:13 -0800

    Add microbenchmarks for compartment transitions

---
+++ benchmarks/microbench/microbench_lib.c
@@ -0,0 +1,5 @@
+#include "microbench_lib.h"
+
+void no_op(void) {}
+
+void no_op_unwrapped(void) {}
---
+++ benchmarks/microbench/microbench_main.c
@@ -0,0 +1,27 @@
+#include "b63/register.h"
+#include <b63/b63.h>
+#include <b63/counters/perf_events.h>
+#include <ia2.h>
+
+#include "microbench_lib.h"
+
+INIT_RUNTIME(1);
+#define IA2_COMPARTMENT 1
+#include <ia2_compartment_init.inc>
+
+B63_BASELINE(no_op, n) {
+  for (size_t i = 0; i < n; ++i) {
+    no_op_unwrapped();
+  }
+}
+
+B63_BENCHMARK(no_op_wrapped, n) {
+  for (size_t i = 0; i < n; ++i) {
+    no_op();
+  }
+}
+
+int main(int argc, char *argv[]) {
+  B63_RUN(argc, argv);
+  return 0;
+}

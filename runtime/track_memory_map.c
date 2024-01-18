475e0d3292df08ad624ead4b27f6f754e2a55963
Frances Wingerter <fw@immunant.com>
2024-01-18 15:38:52 -0500

    runtime/track_memory_map: avoid extra PTRACE_SYSCALL when init is marked finished

--- runtime/track_memory_map.c
+++ runtime/track_memory_map.c
@@ -777,13 +777,7 @@ bool track_memory_map(pid_t pid, int *exit_status_out, enum trace_mode mode) {
       default:
         return false;
       }
-      if (ptrace(PTRACE_SYSCALL, waited_pid, 0, 0) < 0) {
-        perror("could not PTRACE_SYSCALL");
-      }
-      continue;
-    }
-
-    if (!is_op_permitted(map, event, &event_info)) {
+    } else if (!is_op_permitted(map, event, &event_info)) {
       fprintf(stderr, "forbidden operation requested: %s\n", event_name(event));
       return_syscall_eperm(waited_pid);
       if (ptrace(continue_request, waited_pid, 0, 0) < 0) {

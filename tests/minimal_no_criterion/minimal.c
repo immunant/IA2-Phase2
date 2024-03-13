8373dcf8e90a37b22b017b24e2b7d7924445264f
Ayrton Munoz <adm@immunant.com>
2024-03-13 01:00:24 -0400

    tests: Add criterion-free test for aarch64

---
+++ tests/minimal_no_criterion/minimal.c
@@ -0,0 +1,23 @@
+/*
+RUN: cat minimal_call_gates_1.ld | FileCheck --check-prefix=LINKARGS %s
+*/
+
+#include "minimal.h"
+#include <stdio.h>
+
+// LINKARGS: --wrap=arg1
+void arg1(int x) {
+    printf("arg1");
+}
+
+// LINKARGS: --wrap=foo
+void foo() {
+    printf("foo");
+}
+
+// LINKARGS: --wrap=return_val
+int return_val() {
+    printf("return_val");
+    return 1;
+}
+

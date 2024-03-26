0e4ece139e1f553536744438119362699d5a36cf
Ayrton Munoz <adm@immunant.com>
2024-03-25 21:21:38 -0400

    runtime/libia2: Initial scrub registers impl

--- runtime/libia2/include/scrub_registers.h
+++ runtime/libia2/include/scrub_registers.h
@@ -1,4 +1,3 @@
-#if LIBIA2_X86_64
 // This file defines the feature specific scrub routines.
 //
 // This is implemented via the fairly standard fallthrough approach, and
@@ -8,6 +7,7 @@
 // version in particular could be optimized.
 asm(".text\n"
     ".p2align 5\n"
+#if LIBIA2_X86_64
     "__libia2_scrub_registers_avx512:\n"
     "vpxorq %zmm16, %zmm16, %zmm16\n"
     "vpxorq %zmm17, %zmm17, %zmm17\n"
@@ -70,7 +70,39 @@ asm(".text\n"
        support avx512 */
     "jmp __libia2_scrub_registers_sse\n"
     "int3\n"
-    ".previous\n");
 #elif LIBIA2_AARCH64
-#warning "__libia2_scrub_registers is not implemented for aarch64 yet"
+#warning "__libia2_scrub_registers for aarch64 is not complete yet"
+    "__libia2_scrub_registers:\n"
+    "eor x0, x0, x0\n"
+    "eor x1, x1, x1\n"
+    "eor x2, x2, x2\n"
+    "eor x3, x3, x3\n"
+    "eor x4, x4, x4\n"
+    "eor x5, x5, x5\n"
+    "eor x6, x6, x6\n"
+    "eor x7, x7, x7\n"
+    "eor x8, x8, x8\n"
+    "eor x9, x9, x9\n"
+    "eor x10, x10, x10\n"
+    "eor x11, x11, x11\n"
+    "eor x12, x12, x12\n"
+    "eor x13, x13, x13\n"
+    "eor x14, x14, x14\n"
+    "eor x15, x15, x15\n"
+    "eor x16, x16, x16\n"
+    "eor x17, x17, x17\n"
+    // x18
+    "eor x19, x19, x19\n"
+    "eor x20, x20, x20\n"
+    "eor x21, x21, x21\n"
+    "eor x22, x22, x22\n"
+    "eor x23, x23, x23\n"
+    "eor x24, x24, x24\n"
+    "eor x25, x25, x25\n"
+    "eor x26, x26, x26\n"
+    "eor x27, x27, x27\n"
+    "eor x28, x28, x28\n"
+    "ret\n"
+    // x29, x30
 #endif
+    ".previous\n");

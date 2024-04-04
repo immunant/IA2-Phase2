1f10d17bcc8cb3b61d46e2f1c30bd3a5f0ac803c
Ayrton Munoz <adm@immunant.com>
2024-04-04 17:14:39 -0400

    misc/spoofed_criterion: Fork for tests on ARM

--- misc/spoofed_criterion/include/criterion/criterion.h
+++ misc/spoofed_criterion/include/criterion/criterion.h
@@ -2,6 +2,6 @@
 #include <criterion/new/assert.h>
 
 #define Test(suite, name) \
-    void suite##_##name(void); \
-    __attribute__((__section__("fake_criterion_tests"))) void (*suite##_##name##_##test)(void) = suite##_##name; \
-    void suite##_##name(void)
+    int suite##_##name(void); \
+    __attribute__((__section__("fake_criterion_tests"))) int (*suite##_##name##_##test)(void) = suite##_##name; \
+    int suite##_##name(void)

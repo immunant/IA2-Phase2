5ca03c4ac7d6f77eec205209ab8ed3f376b79164
sim-immunant <144717540+sim-immunant@users.noreply.github.com>
2024-04-25 18:55:42 -0700

    Add some missing Criterion stub-outs

--- misc/spoofed_criterion/include/criterion/new/assert.h
+++ misc/spoofed_criterion/include/criterion/new/assert.h
@@ -3,3 +3,7 @@
 #define cr_assert assert
 #define cr_assert_eq(a,b) cr_assert((a) == (b))
 #define cr_fatal assert
+
+
+#define cr_assert_lt(a, b) assert((a) < (b))
+#define cr_assert_eq(a, b) assert((a) == (b))

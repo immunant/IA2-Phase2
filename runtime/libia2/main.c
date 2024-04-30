84e55057fa3ff30739755bcec74eabf2751a0c44
Ayrton Munoz <adm@immunant.com>
2024-04-29 22:54:07 -0400

    runtime/libia2: Fix register sigils in global asm statements

--- runtime/libia2/main.c
+++ runtime/libia2/main.c
@@ -12,34 +12,34 @@ __asm__(
     ".global __wrap_main\n"
     "__wrap_main:\n"
 #if LIBIA2_X86_64
-    "pushq %%rbp\n"
-    "movq %%rsp, %%rbp\n"
+    "pushq %rbp\n"
+    "movq %rsp, %rbp\n"
     // Save the old stack pointer in main_sp.
-    "movq %%rsp, main_sp(%%rip)\n"
+    "movq %rsp, main_sp(%rip)\n"
     // Load the stack pointer for this compartment's stack.
-    "mov ia2_stackptr_1@GOTTPOFF(%%rip), %%r11\n"
-    "mov %%fs:(%%r11), %%rsp\n"
+    "mov ia2_stackptr_1@GOTTPOFF(%rip), %r11\n"
+    "mov %fs:(%r11), %rsp\n"
     // Switch pkey to the appropriate compartment.
-    "xor %%ecx,%%ecx\n"
-    "mov %%ecx,%%edx\n"
+    "xor %ecx,%ecx\n"
+    "mov %ecx,%edx\n"
     "mov_pkru_eax 1\n"
     "wrpkru\n"
     // Align the stack before calling main.
-    "subq $8, %%rsp\n"
+    "subq $8, %rsp\n"
     // Call the real main function.
     "call __real_main\n"
     // Restore the old stack pointer before returning.
-    "mov main_sp(%%rip), %%rsp\n"
+    "mov main_sp(%rip), %rsp\n"
     // Save return value
-    "mov %%rax,%%r10\n"
+    "mov %rax,%r10\n"
     // Switch pkey to untrusted compartment
-    "xor %%ecx,%%ecx\n"
-    "xor %%edx,%%edx\n"
+    "xor %ecx,%ecx\n"
+    "xor %edx,%edx\n"
     "mov_pkru_eax 0\n"
     "wrpkru\n"
     // Restore return value
-    "mov %%r10,%%rax\n"
-    "popq %%rbp\n"
+    "mov %r10,%rax\n"
+    "popq %rbp\n"
     "ret\n"
 #elif LIBIA2_AARCH64
     // prologue

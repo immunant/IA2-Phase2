505911355867afe6934481ea4fc23ab95148f33a
Ayrton Munoz <adm@immunant.com>
2024-05-24 13:07:51 -0400

    misc/spoofed_criterion: Support non-zero exit code and re-enable should_segfault test

--- misc/spoofed_criterion/include/criterion/criterion.h
+++ misc/spoofed_criterion/include/criterion/criterion.h
@@ -1,8 +1,14 @@
 #include <criterion/logging.h>
 #include <criterion/new/assert.h>
 
-#define Test(suite, name) \
-    int fake_criterion_##suite##_##name(void); \
-    __attribute__((__section__("fake_criterion_tests"))) \
-        int (*fake_criterion_##suite##_##name##_##test)(void) = fake_criterion_##suite##_##name; \
-    int fake_criterion_##suite##_##name(void)
+struct fake_criterion_test {
+  int (*test)(void);
+  int exit_code;
+};
+
+#define Test(suite, name, ...)                                                                                                 \
+  int fake_criterion_##suite##_##name(void);                                                                                   \
+  __attribute__((__section__("fake_criterion_tests"))) struct fake_criterion_test fake_criterion_##suite##_##name##_##test = { \
+      .test = fake_criterion_##suite##_##name,                                                                                 \
+      ##__VA_ARGS__};                                                                                                          \
+  int fake_criterion_##suite##_##name(void)

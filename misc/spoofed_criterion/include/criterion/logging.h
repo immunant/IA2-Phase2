5b5b93f17d604a07a361bfb9f20cce661adddeeb
Frances Wingerter <fw@immunant.com>
2024-07-22 14:44:17 -0400

    spoofed_criterion: improve logging support
    we use both cr_log_info and cr_log_error in our tests, so support them both
    
    in addition, they should follow their messages with newlines like the originals do

--- misc/spoofed_criterion/include/criterion/logging.h
+++ misc/spoofed_criterion/include/criterion/logging.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
 #include <criterion/new/assert.h>
 
-#define cr_log_info printf
+#define cr_log_info(f, ...) printf(f "\n", ##__VA_ARGS__)
+#define cr_log_error(f, ...) fprintf(stderr, f "\n", ##__VA_ARGS__)

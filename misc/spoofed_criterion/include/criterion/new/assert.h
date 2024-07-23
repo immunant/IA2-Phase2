2047b9a1062fe8f7fa8b92589791d9f3c4b560e2
Frances Wingerter <fw@immunant.com>
2024-07-22 22:36:14 -0400

    spoofed_criterion: format

--- misc/spoofed_criterion/include/criterion/new/assert.h
+++ misc/spoofed_criterion/include/criterion/new/assert.h
@@ -1,6 +1,10 @@
 #include <assert.h>
 
 #define cr_assert assert
-#define cr_assert_eq(a,b) cr_assert((a) == (b))
-#define cr_assert_lt(a,b) cr_assert((a) < (b))
-#define cr_fatal(s) do {fprintf(stderr, s "\n"); exit(1);} while(0)
+#define cr_assert_eq(a, b) cr_assert((a) == (b))
+#define cr_assert_lt(a, b) cr_assert((a) < (b))
+#define cr_fatal(s)          \
+  do {                       \
+    fprintf(stderr, s "\n"); \
+    exit(1);                 \
+  } while (0)

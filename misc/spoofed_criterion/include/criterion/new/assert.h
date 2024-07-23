69f1bbba7c5a1fbc8c49e1b916f13871c43e5ec2
Frances Wingerter <fw@immunant.com>
2024-07-22 22:35:50 -0400

    spoofed_criterion: correctly implement cr_fatal

--- misc/spoofed_criterion/include/criterion/new/assert.h
+++ misc/spoofed_criterion/include/criterion/new/assert.h
@@ -3,4 +3,4 @@
 #define cr_assert assert
 #define cr_assert_eq(a,b) cr_assert((a) == (b))
 #define cr_assert_lt(a,b) cr_assert((a) < (b))
-#define cr_fatal assert
+#define cr_fatal(s) do {fprintf(stderr, s "\n"); exit(1);} while(0)

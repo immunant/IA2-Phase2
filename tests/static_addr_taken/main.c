5dce4e07fa7e933cfaa52075d5f1f3853ddef9c1
Stephen Crane <sjc@immunant.com>
2024-10-14 18:31:47 -0700

    Disable identically named static function

--- tests/static_addr_taken/main.c
+++ tests/static_addr_taken/main.c
@@ -16,13 +16,13 @@ LOCAL void macro_attr_noop(void) {
     printf("called %s in main binary\n", __func__);
 }
 
-static void identical_name(void) {
-    static int x = 3;
-    printf("%s in main binary read x = %d\n", __func__, x);
-}
+// static void identical_name(void) {
+//     static int x = 3;
+//     printf("%s in main binary read x = %d\n", __func__, x);
+// }
 
 static fn_ptr_ty ptrs[] IA2_SHARED_DATA = {
-    inline_noop, duplicate_noop, identical_name, macro_attr_noop,
+    inline_noop, duplicate_noop, /* identical_name, */ macro_attr_noop,
 };
 
 fn_ptr_ty *get_ptrs_in_main(void) {

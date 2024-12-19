bb6f17cae7c398074108ded051f28f9cee7a0676
Khyber Sen <kkysen@gmail.com>
2024-12-19 13:32:11 -0800

    tests: split `post_condition` `main` test into `normal` and `corrupt_bounds` tests to have a control
    `exit(128 + SIGABRT)` is temporarily used instead of `assert` (which `raise`s `SIGABRT`)
    due to `ia2-sandbox`'s `ptrace` tracer not yet handling it.
    That will be fixed shortly.

--- tests/post_condition/dav1d.c
+++ tests/post_condition/dav1d.c
@@ -4,10 +4,16 @@ RUN: cat dav1d_call_gates_1.ld | FileCheck --check-prefix=LINKARGS %s
 
 #include "dav1d.h"
 #include <ia2_test_runner.h>
+#include <signal.h>
+
+bool corrupt_stride IA2_SHARED_DATA = false;
 
 // LINKARGS: --wrap=dav1d_get_picture
 int dav1d_get_picture(Dav1dContext *const c, Dav1dPicture *const out) {
-  out->stride[0] = -1;
+  out->stride[0] = 1;
+  if (corrupt_stride) {
+    out->stride[0] *= -1;
+  }
   return 0;
 }
 
@@ -15,5 +21,8 @@ void dav1d_get_picture_post_condition(Dav1dContext *const c, Dav1dPicture *const
   cr_log_info("dav1d_get_picture post condition ran");
   if (out->stride[0] < 0) {
     cr_log_info("negative stride");
+    // signals, like the `SIGABRT` from `assert` don't yet work with
+    // `ia2-sandbox` and its `ptrace` tracer.
+    exit(128 + SIGABRT);
   }
 }

730759d39bcf3433b33d0dfb9f53f5c4d3fccad8
Khyber Sen <kkysen@gmail.com>
2025-02-18 01:43:03 -0800

    tests: add `pre_post_condition`, testing a single post-condition function for now
    It also tests pre- and post-condition functions
    for a non-existent function, which shouldn't error.

---
+++ tests/pre_post_condition/src.c
@@ -0,0 +1,27 @@
+#include "src.h"
+#include <ia2_test_runner.h>
+#include <signal.h>
+
+#define IA2_COMPARTMENT 2
+#include <ia2_compartment_init.inc>
+
+void f1(int a, int *b) {
+  *b = a;
+}
+
+IA2_POST_CONDITION_FOR(f1)
+void b_eq_10(int a, int *b) {
+  if (!(*b == 10)) {
+    exit(21);
+  }
+}
+
+IA2_PRE_CONDITION_FOR(f2)
+void exit_30(void) {
+  exit(30);
+}
+
+IA2_POST_CONDITION_FOR(f2)
+void exit_31(void) {
+  exit(31);
+}

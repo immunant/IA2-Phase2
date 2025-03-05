c652729ca9a3a69099ad503a6c2bb506fe15297a
Frances Wingerter <fw@immunant.com>
2025-03-05 12:18:27 -0800

    rewriter: silence LLVM 14 warnings with g++

--- tools/rewriter/DetermineAbi.h
+++ tools/rewriter/DetermineAbi.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "CAbi.h"
 #include "GenCallAsm.h"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-enum-enum-conversion"
+#pragma GCC diagnostic ignored "-Wnonnull"
 #include "clang/AST/AST.h"
+#pragma GCC diagnostic pop
 
 auto determineAbiForDecl(const clang::FunctionDecl &fnDecl, Arch arch) -> AbiSignature;
 

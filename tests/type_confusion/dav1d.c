ae8bd68c5fab2c6323ce9d3c0cbb3de9a0d8e903
Khyber Sen <kkysen@gmail.com>
2025-03-07 14:03:18 -0800

    tests: remove unused `LINKARGS` in `type_confusion`

--- tests/type_confusion/dav1d.c
+++ tests/type_confusion/dav1d.c
@@ -8,17 +8,14 @@ RUN: cat dav1d_call_gates_1.ld | FileCheck --check-prefix=LINKARGS %s
 #define IA2_COMPARTMENT 2
 #include <ia2_compartment_init.inc>
 
-// LINKARGS: --wrap=dav1d_open
 int dav1d_open(Dav1dContext **const c_out, const Dav1dSettings *const s) {
   return 0;
 }
 
-// LINKARGS: --wrap=dav1d_close
 void dav1d_close(Dav1dContext **const c_out) {
   return;
 }
 
-// LINKARGS: --wrap=dav1d_get_picture
 int dav1d_get_picture(Dav1dContext *const c, Dav1dPicture *const out) {
   return 0;
 }

47883fed82348bcb158c2807630306caff48b8ca
Khyber Sen <kkysen@gmail.com>
2025-03-09 02:05:56 -0700

    rewriter: add `Structors` pass to find {con,de}structors

--- tools/rewriter/Context.h
+++ tools/rewriter/Context.h
@@ -9,10 +9,14 @@ typedef std::string Function;
 
 class Context {
 public:
-  /// Key is target function.
+  /// Key is target function name.
   /// Value is pre/post condition function name.
   std::unordered_multimap<Function, Function> pre_condition_funcs;
   std::unordered_multimap<Function, Function> post_condition_funcs;
 
   TypeInfoInterner types;
+
+  /// Key is function name.
+  std::unordered_map<std::string, TypeId> constructors;
+  std::unordered_map<std::string, TypeId> destructors;
 };

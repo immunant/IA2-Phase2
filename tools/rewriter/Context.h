92d0ba33a8fb5da74267e992cfb49cc718972b8f
Khyber Sen <kkysen@gmail.com>
2025-03-09 01:16:01 -0700

    rewriter: add a `TypeId` typedef

--- tools/rewriter/Context.h
+++ tools/rewriter/Context.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "TypeOps.h"
+
 #include <string>
 #include <unordered_map>
 
@@ -12,5 +14,5 @@ public:
 
   /// Key is canonical type name.
   /// Value is type ID.
-  std::unordered_map<std::string, uint32_t> type_ids;
+  std::unordered_map<std::string, TypeId> type_ids;
 };

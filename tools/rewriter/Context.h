2708ded090258428b504be521ec606710105af86
Khyber Sen <kkysen@gmail.com>
2025-03-09 01:10:50 -0700

    rewriter: move `type_ids` global to `Context`

--- tools/rewriter/Context.h
+++ tools/rewriter/Context.h
@@ -9,4 +9,8 @@ public:
   /// Value is pre/post condition function name.
   std::unordered_multimap<std::string, std::string> pre_condition_funcs;
   std::unordered_multimap<std::string, std::string> post_condition_funcs;
+
+  /// Key is canonical type name.
+  /// Value is type ID.
+  std::unordered_map<std::string, uint32_t> type_ids;
 };

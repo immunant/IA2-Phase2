ebce1b7e2c2850e7f0f011b40e512424c53ed0e6
Ayrton Munoz <adm@immunant.com>
2025-05-12 15:31:41 -0700

    tests: Update two_keys_minimal test to use new init method

--- tests/two_keys_minimal/main.c
+++ tests/two_keys_minimal/main.c
@@ -19,6 +19,11 @@ INIT_RUNTIME(2);
 #define IA2_COMPARTMENT 1
 #include <ia2_compartment_init.inc>
 
+void ia2_start(void) {
+    ia2_protect_memory("main", 1);
+    ia2_protect_memory("libtwo_keys_minimal_lib.so", 2);
+}
+
 uint32_t secret = 0x09431233;
 
 static bool steal_plugin_secret = false;

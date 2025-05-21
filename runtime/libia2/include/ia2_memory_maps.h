12c1ea544e39e8da48a76510caa67601279515ba
Khyber Sen <kkysen@gmail.com>
2025-05-20 23:55:40 -0700

    libia2/memory_maps: add docs for `ia2_log_memory_map`

--- runtime/libia2/include/ia2_memory_maps.h
+++ runtime/libia2/include/ia2_memory_maps.h
@@ -2,4 +2,6 @@
 
 #include <stdio.h>
 
+/// Log the labeled memory maps from `/proc/self/maps` to `log`.
+/// This can be called at any point, including in a debugger.
 void ia2_log_memory_maps(FILE* log);

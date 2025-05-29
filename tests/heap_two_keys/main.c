1db439f1feb38a86c626743c78c3a42a16baa51f
Ayrton Munoz <adm@immunant.com>
2025-05-29 13:00:32 -0400

    tests: Add ia2_main

--- tests/heap_two_keys/main.c
+++ tests/heap_two_keys/main.c
@@ -17,6 +17,11 @@ INIT_RUNTIME(2);
 #define IA2_COMPARTMENT 1
 #include <ia2_compartment_init.inc>
 
+void ia2_main(void) {
+    ia2_register_compartment("main", 1, NULL);
+    ia2_register_compartment("libheap_two_keys_lib.so", 2, NULL);
+}
+
 // Test that the program can exit without error
 Test(heap_two_keys, 0, .init = trigger_compartment_init) {
 }
--- tests/rewrite_fn_ptr_eq/main.c
+++ tests/rewrite_fn_ptr_eq/main.c
@@ -10,6 +10,10 @@ INIT_RUNTIME(1);
 #define IA2_COMPARTMENT 1
 #include <ia2_compartment_init.inc>
 
+void ia2_main(void) {
+    ia2_register_compartment("main", 1, NULL);
+}
+
 int add(int x, int y) {
     return x + y;
 }

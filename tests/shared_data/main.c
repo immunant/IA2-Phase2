75de4fbf0f1e4409e8ada7cf7e18c7812a88487c
Ayrton Munoz <adm@immunant.com>
2025-05-29 13:00:32 -0400

    tests: Add ia2_main

--- tests/shared_data/main.c
+++ tests/shared_data/main.c
@@ -11,6 +11,10 @@ INIT_RUNTIME(1);
 #define IA2_COMPARTMENT 1
 #include <ia2_compartment_init.inc>
 
+void ia2_main(void) {
+    ia2_register_compartment("main", 1, NULL);
+}
+
 uint8_t shared_val[4097] IA2_SHARED_DATA = { 0 };
 
 void check_shared_access(uint8_t *shared) {

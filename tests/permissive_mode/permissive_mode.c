9982abc14dc554a8a26d8364f062d08256c38c34
Ayrton Munoz <adm@immunant.com>
2025-06-02 22:45:59 -0700

    tests/permissive_mode: Remove pkey_alloc in test
    Runtime init now allocates all pkeys for simplicity so the test no longer needs
    to do this.

--- tests/permissive_mode/permissive_mode.c
+++ tests/permissive_mode/permissive_mode.c
@@ -19,9 +19,6 @@ Test(permissive_mode, main) {
     char* buffer = NULL;
     cr_assert(ia2_get_tag() == 0xFFFFFFF0);
 
-    /* allocate an extra pkey */
-    cr_assert(pkey_alloc(0, PKEY_DISABLE_ACCESS | PKEY_DISABLE_WRITE) == 2);
-
     buffer = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
 
     pkey_mprotect(buffer, 4096, PROT_READ | PROT_WRITE, 1);

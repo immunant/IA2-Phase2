272d268f1c9cdb9cb0e02083dcdf1990ebc78885
Ayrton Munoz <adm@immunant.com>
2025-06-02 22:41:08 -0700

    runtime/libia2: Remove rdpkru check in protect_pages

--- runtime/libia2/ia2.c
+++ runtime/libia2/ia2.c
@@ -374,12 +374,6 @@ int protect_pages(struct dl_phdr_info *info, size_t size, void *data) {
 
   struct PhdrSearchArgs *search_args = (struct PhdrSearchArgs *)data;
 
-  size_t cur_pkey = ia2_get_compartment();
-  if (cur_pkey != search_args->pkey) {
-    fprintf(stderr, "Invalid pkey, expected %" PRId32 ", found %zu\n",
-            search_args->pkey, cur_pkey);
-    abort();
-  }
   Elf64_Addr address = (Elf64_Addr)search_args->address;
   bool extra = in_extra_libraries(info, search_args->extra_libraries);
   if (!in_loaded_segment(info, address) && !extra) {
@@ -511,7 +505,7 @@ int protect_pages(struct dl_phdr_info *info, size_t size, void *data) {
         // TODO: Inline ia2_mprotect_with_tag call and make sure the pkey is in a
         // register here so we can disallow calls to the libc function
         int mprotect_err = ia2_mprotect_with_tag((void *)start, cur_end - start,
-                                         access_flags, (int)cur_pkey);
+                                         access_flags, (int)search_args->pkey);
         if (mprotect_err != 0) {
           printf("ia2_mprotect_with_tag failed: %s\n", strerror(errno));
           exit(-1);

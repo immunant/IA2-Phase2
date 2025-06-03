6b6b389803e87de8640bc972857f1c8837db2873
Ayrton Munoz <adm@immunant.com>
2025-06-03 01:08:18 -0400

    external/nginx: Add ia2_main

--- external/nginx/src/core/nginx.c
+++ external/nginx/src/core/nginx.c
@@ -15,6 +15,12 @@ INIT_RUNTIME(2);
 #define IA2_COMPARTMENT 1
 #include <ia2_compartment_init.inc>
 
+void ia2_main(void) {
+#if IA2_ENABLE
+    ia2_register_compartment("main", 1, NULL);
+    ia2_register_compartment("ngx_rtmp_module_tls_padded.so", 2, NULL);
+#endif
+}
 
 static void ngx_show_version_info(void);
 static ngx_int_t ngx_add_inherited_sockets(ngx_cycle_t *cycle);

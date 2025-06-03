cf6f2ec41fe12bbb04fe45aa6465b94161600de4
Ayrton Munoz <adm@immunant.com>
2025-06-03 00:22:19 -0400

    benchmarks: Add ia2_main

--- benchmarks/microbench/microbench_indirect.c
+++ benchmarks/microbench/microbench_indirect.c
@@ -10,6 +10,11 @@ INIT_RUNTIME(2);
 #define IA2_COMPARTMENT 1
 #include <ia2_compartment_init.inc>
 
+void ia2_main(void) {
+    ia2_register_compartment("main", 1, NULL);
+    ia2_register_compartment("libmicrobenchmark_indirect2.so", 2, NULL);
+}
+
 void (*fn_ptr)(void);
 
 IA2_BEGIN_NO_WRAP

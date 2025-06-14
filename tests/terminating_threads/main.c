5ba036a1b2fd74053be56deb4a13b09aa0f28b6f
Khyber Sen <kkysen@gmail.com>
2025-06-14 04:44:01 -0700

    tests/terminating_threads: add a test for various combinations of ways threads can terminate
    This tests various combinations of threads terminating,
    including a matrix of:
    * 1, 2, and 11 threads (including the main thread)
    * threads terminating by `return`, `exit(0)`, `abort()`, `pthread_exit(0)`
      (including the main thread) or `pause()`ing or `sleep`ing
    * threads being `pthread_join`ed, `pthread_cancel`ed, or through process termination
    
    However, `abort()` currently incurs a ~1 second penalty for some reason, so that's disabled.
    Moreover, `pthread_exit` causes `SIGILL`
    and `pthread_cancel` causes `SIGSEGV`, so these are disabled, too.
    
    Furthermore, just like the other existing thread tests (`threads` and `protected_threads`),
    `terminating_threads` runs `WITHOUT_SANDBOX`, as the tracer fails on all of them.

---
+++ tests/terminating_threads/main.c
@@ -0,0 +1,186 @@
+#include <ia2.h>
+#include <ia2_test_runner.h>
+
+INIT_RUNTIME(2);
+#define IA2_COMPARTMENT 1
+#include <ia2_compartment_init.inc>
+
+#include <pthread.h>
+// #include <signal.h> // TODO 1 second delay from SIGABRT
+
+#if IA2_ENABLE
+
+typedef void *(*start_fn)(void *arg);
+typedef int (*end_fn)(pthread_t thread);
+
+void *start_return(void *_arg) {
+  return NULL;
+}
+
+void *start_exit(void *_arg) {
+  exit(0);
+}
+
+void *start_abort(void *_arg) {
+  // abort(); // TODO 1 second delay
+  return NULL;
+}
+
+void *start_pthread_exit(void *_arg) {
+  // pthread_exit(NULL); // TODO `SIGILL`s.
+  return NULL;
+}
+
+void *start_pause(void *_arg) {
+  pause();
+  return NULL;
+}
+
+void *start_sleep_100_us(void *_arg) {
+  usleep(100);
+  return NULL;
+}
+
+int end_none(pthread_t _thread) {
+  return 0;
+}
+
+int end_join(pthread_t thread) {
+  return pthread_join(thread, NULL);
+}
+
+int end_cancel(pthread_t thread) {
+  // return pthread_cancel(thread); // TODO `SIGSEGV`s.
+  exit(0);
+}
+
+void run_test(size_t num_threads, start_fn start, end_fn end, start_fn main) {
+  if (num_threads > 0) {
+    pthread_t threads[num_threads];
+    for (size_t i = 0; i < num_threads; i++) {
+      pthread_create(&threads[i], NULL, start, NULL);
+    }
+    for (size_t i = 0; i < num_threads; i++) {
+      cr_assert(end(threads[i]) == 0);
+    }
+  }
+  main(NULL);
+}
+
+// 1 thread
+
+Test(terminating_threads, threads_1_return) {
+  run_test(0, start_pause, end_none, start_return);
+}
+
+Test(terminating_threads, threads_1_exit) {
+  run_test(0, start_pause, end_none, start_exit);
+}
+
+Test(terminating_threads, threads_1_abort,
+     // .signal = SIGABRT
+) {
+  run_test(0, start_pause, end_none, start_abort);
+}
+
+Test(terminating_threads, threads_1_pthread_exit) {
+  run_test(0, start_pause, end_none, start_pthread_exit);
+}
+
+// 2 threads, main thread
+
+Test(terminating_threads, threads_2_main_thread_return) {
+  run_test(1, start_pause, end_none, start_return);
+}
+
+Test(terminating_threads, threads_2_main_thread_exit) {
+  run_test(1, start_pause, end_none, start_exit);
+}
+
+Test(terminating_threads, threads_2_main_thread_abort,
+     // .signal = SIGABRT
+) {
+  run_test(1, start_pause, end_none, start_abort);
+}
+
+Test(terminating_threads, threads_2_main_thread_pthread_exit) {
+  run_test(1, start_pause, end_none, start_pthread_exit);
+}
+
+// 11 threads, main thread
+
+Test(terminating_threads, threads_11_main_thread_return) {
+  run_test(10, start_pause, end_none, start_return);
+}
+
+Test(terminating_threads, threads_11_main_thread_exit) {
+  run_test(10, start_pause, end_none, start_exit);
+}
+
+Test(terminating_threads, threads_11_main_thread_abort,
+     // .signal = SIGABRT
+) {
+  run_test(10, start_pause, end_none, start_abort);
+}
+
+Test(terminating_threads, threads_11_main_thread_pthread_exit) {
+  run_test(10, start_pause, end_none, start_pthread_exit);
+}
+
+// 2 threads, other thread
+
+Test(terminating_threads, threads_2_other_thread_return) {
+  run_test(1, start_return, end_join, start_return);
+}
+
+Test(terminating_threads, threads_2_other_thread_exit) {
+  run_test(1, start_exit, end_join, start_return);
+}
+
+Test(terminating_threads, threads_2_other_thread_abort,
+     // .signal = SIGABRT
+) {
+  run_test(1, start_abort, end_join, start_return);
+}
+
+Test(terminating_threads, threads_2_other_thread_pthread_exit) {
+  run_test(1, start_pthread_exit, end_join, start_return);
+}
+
+Test(terminating_threads, threads_2_other_thread_pthread_join) {
+  run_test(1, start_sleep_100_us, end_join, start_return);
+}
+
+Test(terminating_threads, threads_2_other_thread_pthread_cancel) {
+  run_test(1, start_pause, end_cancel, start_return);
+}
+
+// 11 threads, other threads
+
+Test(terminating_threads, threads_11_other_threads_return) {
+  run_test(10, start_return, end_join, start_return);
+}
+
+Test(terminating_threads, threads_11_other_threads_exit) {
+  run_test(10, start_exit, end_join, start_return);
+}
+
+Test(terminating_threads, threads_11_other_threads_abort,
+     // .signal = SIGABRT
+) {
+  run_test(10, start_abort, end_join, start_return);
+}
+
+Test(terminating_threads, threads_11_other_threads_pthread_exit) {
+  run_test(10, start_pthread_exit, end_join, start_return);
+}
+
+Test(terminating_threads, threads_11_other_threads_pthread_join) {
+  run_test(10, start_sleep_100_us, end_join, start_return);
+}
+
+Test(terminating_threads, threads_11_other_threads_pthread_cancel) {
+  run_test(10, start_pause, end_cancel, start_return);
+}
+
+#endif

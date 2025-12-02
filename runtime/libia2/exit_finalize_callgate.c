17e08152256991b39c5072e9f789190fc130dd1d
David Anekstein <aneksteind@gmail.com>
2025-12-02 18:22:21 -0500

    runtime: drop exit finalize stub

--- runtime/libia2/exit_finalize_callgate.c
+++
@@ -1,8 +0,0 @@
-#if defined(IA2_LIBC_COMPARTMENT) && IA2_LIBC_COMPARTMENT
-#include "ia2_internal.h"
-
-#include <stddef.h>
-
-extern void __real___cxa_finalize(void *dso_handle);
-
-#endif // IA2_LIBC_COMPARTMENT

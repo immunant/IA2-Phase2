5b896b14d5c902f5767fc567dfa73398468d5637
David Anekstein <da@immunant.com>
2026-03-20 18:41:04 -0400

    tracer: allow non-fixed mmap hints; add deterministic regression test

--- tests/three_keys_minimal/include/lib_2/lib_2.h
+++ tests/three_keys_minimal/include/lib_2/lib_2.h
@@ -3,3 +3,15 @@
 #include "lib.h"
 
 DECLARE_LIB(2, 1);
+
+/**
+ * Issue a non-MAP_FIXED anonymous mmap using an address hint from another
+ * compartment.
+ *
+ * This is used by the regression test for tracer mmap policy. The kernel may
+ * ignore a non-fixed hint and place the mapping elsewhere, so this operation
+ * should be allowed by policy.
+ *
+ * @return 0 on success, or -errno on failure.
+ */
+int lib_2_mmap_nonfixed_hint(void *hint);

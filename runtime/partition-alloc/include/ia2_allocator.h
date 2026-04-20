33a61d7b966691c51f6341ce983be237a518b15a
David Anekstein <da@immunant.com>
2026-04-20 13:42:05 -0400

    allocator: remove unused extern pkey macro
    The shared allocator declarations no longer emit
    ia2_extern_pkey annotations. Remove the empty macro and its
    no-op uses.

--- runtime/partition-alloc/include/ia2_allocator.h
+++ runtime/partition-alloc/include/ia2_allocator.h
@@ -5,37 +5,26 @@
 extern "C" {
 #endif
 
-#define IA2_EXTERN_PKEY_ANNOTATION(pkey)
-
 __attribute__((visibility("default"), noinline))
-IA2_EXTERN_PKEY_ANNOTATION(1)
 void *shared_malloc(size_t bytes);
 
 __attribute__((visibility("default"), noinline))
-IA2_EXTERN_PKEY_ANNOTATION(1)
 void shared_free(void *ptr);
 
 __attribute__((visibility("default"), noinline))
-IA2_EXTERN_PKEY_ANNOTATION(1)
 void *shared_realloc(void *ptr, size_t size);
 
 __attribute__((visibility("default"), noinline))
-IA2_EXTERN_PKEY_ANNOTATION(1)
 void *shared_calloc(size_t num, size_t size);
 
 __attribute__((visibility("default"), noinline))
-IA2_EXTERN_PKEY_ANNOTATION(1)
 void *shared_memalign(size_t algin, size_t size);
 
 __attribute__((visibility("default"), noinline))
-IA2_EXTERN_PKEY_ANNOTATION(1)
 int shared_posix_memalign(void **res, size_t align, size_t size);
 
 __attribute__((visibility("default"), noinline))
-IA2_EXTERN_PKEY_ANNOTATION(1)
 char* shared_strdup(const char* str);
-
-#undef IA2_EXTERN_PKEY_ANNOTATION
 
 #ifdef __cplusplus
 } // extern "C"
